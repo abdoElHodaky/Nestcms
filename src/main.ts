d7ce69cd5fb6acb17ce65151434837e2d05fc034
AbdElrhman ElHodaky <47593167+abdoElHodaky@users.noreply.github.com>
2024-03-18 16:11:57 +0200

    Update main.ts

--- src/main.ts
+++ src/main.ts
@@ -1,8 +1,18 @@
 import { NestFactory } from '@nestjs/core';
+import { SwaggerModule, DocumentBuilder } from '@nestjs/swagger';
 import { AppModule } from './app.module';
 
 async function bootstrap() {
   const app = await NestFactory.create(AppModule);
+const options = new DocumentBuilder()
+    .setTitle('nestcmsapi')
+    .setDescription('')
+    .setVersion('1.0')
+    .addServer('/', '')
+    .build();
+
+const document = SwaggerModule.createDocument(app, options);
+  SwaggerModule.setup('api-docs', app, document);
   app.enableCors();
   await app.listen(3000);
 }
