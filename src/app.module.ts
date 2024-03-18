16f507c69bd2408d930e3de2d4668ab8d46a97b6
AbdElrhman ElHodaky <47593167+abdoElHodaky@users.noreply.github.com>
2024-03-18 13:19:26 +0200

    Update app.module.ts

--- src/app.module.ts
+++ src/app.module.ts
@@ -6,11 +6,11 @@ import { ArticlesModule } from './articles/articles.module';
 import { AuthModule } from './auth/auth.module';
 import { UsersModule } from './users/users.module';
 
-import config from './config';
+//import config from './config';
 
 @Module({
   imports: [
-    MongooseModule.forRoot(config.mongoUri, { useNewUrlParser: true, useFindAndModify: false }),
+    MongooseModule.forRoot("mongodb+srv://abdoarh36:TyWF4ABOefQhJFbP@cluster0.bc7sxu7.mongodb.net/nestcms", { useNewUrlParser: true, useFindAndModify: false }),
     ArticlesModule,
     AuthModule,
     UsersModule,
