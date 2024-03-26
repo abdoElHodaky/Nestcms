57a1c3fe3cc3cd8559b64c0c6cf324c7e107f4d8
AbdElrhman ElHodaky <47593167+abdoElHodaky@users.noreply.github.com>
2024-03-26 21:20:30 +0200

    Update contract.schema.ts

--- src/contracts/models/contract.schema.ts
+++ src/contracts/models/contract.schema.ts
@@ -7,7 +7,7 @@ export const ContractSchema = new mongoose.Schema({
   creationDate: String,
   status:String,
   path:String,
-  offerId: { type: mongoose.Schema.Types.ObjectId, ref: 'Offer'},
-  clientId: { type: mongoose.Schema.Types.ObjectId, ref: 'User'},
-  employeeId: { type: mongoose.Schema.Types.ObjectId, ref: 'User'}
+  offer: { type: mongoose.Schema.Types.ObjectId, ref: 'Offer'},
+  client: { type: mongoose.Schema.Types.ObjectId, ref: 'User'},
+  employee: { type: mongoose.Schema.Types.ObjectId, ref: 'User'}
 },{timestamp:true});
