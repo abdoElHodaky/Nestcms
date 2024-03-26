4ef9ea5c8dd359f21a2fb4b43a21e4b4b500cb25
AbdElrhman ElHodaky <47593167+abdoElHodaky@users.noreply.github.com>
2024-03-26 18:46:28 +0200

    Update projects.service.ts

--- src/projects/projects.service.ts
+++ src/projects/projects.service.ts
@@ -1,1 +1,19 @@
+import { Injectable } from '@nestjs/common';
+import { Model } from 'mongoose';
+import { InjectModel } from '@nestjs/mongoose';
+import { CreateProjectDto } from './dto/create-project.dto';
+import { Project } from './interfaces/project.interface';
 
+@Injectable()
+export class ProjectService {
+  constructor(@InjectModel('Project') private readonly projectModel: Model<Project>) {}
+
+ /* async create(createUserDto: CreateUserDto): Promise<Project> {
+    const createdUser = new this.userModel(createUserDto);
+    return await createdUser.save();
+  }
+
+  async findOne(email: string): Promise<User> {
+    return await this.userModel.findOne({ email }, '-__v').exec();
+  } */
+}
