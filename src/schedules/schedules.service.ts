3b247c96e237877046f0b1bd7d5e8b5ad331b5ec
AbdElrhman ElHodaky <47593167+abdoElHodaky@users.noreply.github.com>
2024-03-27 00:35:02 +0200

    Update schedules.service.ts

--- src/schedules/schedules.service.ts
+++ src/schedules/schedules.service.ts
@@ -3,14 +3,18 @@ import { Model } from 'mongoose';
 import { InjectModel } from '@nestjs/mongoose';
 import { CreateScheduleDto } from './dto/create-schedule.dto';
 import { Schedule } from './interface/schedule.interface';
-
+import { UsersService} from "../users/users.service"
 @Injectable()
 export class ScheduleService {
   constructor(@InjectModel('Schedule') private readonly scheduleModel: Model<Schedule>) {}
-
+  private userService:UsersService
  
   async create(createScheduleDto: CreateScheduleDto): Promise<Project> {
     const createdSchedule = new this.scheduleModel(createScheduleDto);
+    const client =await this.userService.find_Id(createScheduleDto.clientId)
+    const employee = await this.userService.find_Id(createScheduleDto.employeeId)
+    createdSchedule.client=client 
+    createdSchedule.employee=employee
     return await createdSchedule.save();
   }
 /*
