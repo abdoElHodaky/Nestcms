fb216c3e2c2c4d3633ed26968573ba9c740e82b6
AbdElrhman ElHodaky <47593167+abdoElHodaky@users.noreply.github.com>
2024-03-26 17:33:41 +0200

    Update user.schema.ts

--- src/users/models/user.schema.ts
+++ src/users/models/user.schema.ts
@@ -3,4 +3,8 @@ import * as mongoose from 'mongoose';
 export const UserSchema = new mongoose.Schema({
   email: String,
   password: String,
+  type:String,
+  fullName:String,
+  Age:Number,
+  Addresse:String
 });
