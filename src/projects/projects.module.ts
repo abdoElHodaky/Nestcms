ffcc5d45fc9716e6df9a92f8f2a40c9ae7034be5
AbdElrhman ElHodaky <47593167+abdoElHodaky@users.noreply.github.com>
2024-03-26 18:42:39 +0200

    Update projects.module.ts

--- src/projects/projects.module.ts
+++ src/projects/projects.module.ts
@@ -1,1 +1,13 @@
+import { Module } from '@nestjs/common';
+import { ProjectService } from './projects.service';
+import { ProjectController } from './projects.controller';
+import { MongooseModule } from '@nestjs/mongoose';
+import { ProjectSchema } from './models/project.schema';
 
+@Module({
+  imports: [MongooseModule.forFeature([{ name: 'Project', schema: ProjectSchema }])],
+  providers: [ProjectService],
+  exports: [ProjectService],
+  controllers: [ProjectController],
+})
+export class ProjectsModule {}
