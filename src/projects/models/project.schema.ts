075d6943928d243b711047b81ecc658f1cd22315
AbdElrhman ElHodaky <47593167+abdoElHodaky@users.noreply.github.com>
2024-03-27 12:08:24 +0200

    Update project.schema.ts

--- src/projects/models/project.schema.ts
+++ src/projects/models/project.schema.ts
@@ -8,5 +8,6 @@ export const ProjectSchema = new mongoose.Schema({
   endDate:String,
   status:String,
   client: { type: mongoose.Schema.Types.ObjectId, ref: 'User'},
-  employee: { type: mongoose.Schema.Types.ObjectId, ref: 'User'}
+  employee: { type: mongoose.Schema.Types.ObjectId, ref: 'User'},
+  designs:[{ type: mongoose.Schema.Types.ObjectId}, ref:"Design"],
 },{timestamp:true});
