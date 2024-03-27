ddca10e8498fef73add26190e770cf7707df106f
AbdElrhman ElHodaky <47593167+abdoElHodaky@users.noreply.github.com>
2024-03-27 12:10:55 +0200

    Update projects.module.ts

--- src/projects/projects.module.ts
+++ src/projects/projects.module.ts
@@ -3,9 +3,10 @@ import { ProjectService } from './projects.service';
 import { ProjectController } from './projects.controller';
 import { MongooseModule } from '@nestjs/mongoose';
 import { ProjectSchema } from './models/project.schema';
+import { DesignSchema } from './models/design.schema';
 
 @Module({
-  imports: [MongooseModule.forFeature([{ name: 'Project', schema: ProjectSchema }])],
+  imports: [MongooseModule.forFeature([{ name: 'Project', schema: ProjectSchema },{ name :"Design" , schema :Design}])],
   providers: [ProjectService],
   exports: [ProjectService],
   controllers: [ProjectController],
