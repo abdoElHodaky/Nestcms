774566f1d6c46f769862c634a1f09cf2e60fbf6f
AbdElrhman ElHodaky <47593167+abdoElHodaky@users.noreply.github.com>
2024-03-27 17:36:23 +0200

    Update design.interface.ts

--- src/projects/interface/design.interface.ts
+++ src/projects/interface/design.interface.ts
@@ -1,8 +1,9 @@
+import { Project } from "./project.interface";
 export class Design {
   _id:string;
   desc:string;
   path:string;
   created_at?:string;
   updated_at?:string;
-  project?:{};
+  project?:Project;
 }
