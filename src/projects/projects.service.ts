7fc026d09e77c7867061dc6e6f73e19bea8549ca
AbdElrhman ElHodaky <47593167+abdoElHodaky@users.noreply.github.com>
2024-03-27 12:59:54 +0200

    Update projects.service.ts

--- src/projects/projects.service.ts
+++ src/projects/projects.service.ts
@@ -1,17 +1,31 @@
 import { Injectable } from '@nestjs/common';
-import { Model } from 'mongoose';
+import { Model,Types } from 'mongoose';
 import { InjectModel } from '@nestjs/mongoose';
 import { CreateProjectDto } from './dto/create-project.dto';
 import { Project } from './interface/project.interface';
-
+import { CreateDesignDto } from './dto/create-design.dto';
+import { Design } from './interface/design.interface'
 @Injectable()
 export class ProjectService {
-  constructor(@InjectModel('Project') private readonly projectModel: Model<Project>) {}
+  constructor(@InjectModel('Project') private readonly projectModel: Model<Project>,
+              @InjectModel('Design') private readonly designModel: Model<Design>
+             ) {}
 
  
   async create(createProjectDto: CreateProjectDto): Promise<Project> {
     const createdProject = new this.projectModel(createProjectDto);
     return await createdProject.save();
+  }
+
+  async addDesign(id:string,createDesignDto:CreateDesignDto):Promise<Project>{
+    
+    const project=await this.projectModel.findById(new Types.ObjectId(id)).exec()
+    const design =new this.designModel(createDesignDto);
+    design.project=project
+    await design.save()
+    project.designs.push(design)
+    return project
+    
   }
 /*
   async findOne(email: string): Promise<User> {
