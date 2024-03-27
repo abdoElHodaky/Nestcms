7d06c7961c241243663b9d90ccf0297e9cdd6631
AbdElrhman ElHodaky <47593167+abdoElHodaky@users.noreply.github.com>
2024-03-27 13:06:24 +0200

    Create designs.controller.ts

---
+++ src/projects/designs.controller.ts
@@ -0,0 +1,34 @@
+import { Controller, Post, Body, Get, Delete, Param, UseInterceptors, Put,UseGuards,Request } from '@nestjs/common';
+import { AuthGuard } from '@nestjs/passport';
+import { ProjectService } from "./projects.service";
+@UseGuards(AuthGuard('jwt'))
+@Controller('api/projects/:id')
+export class ProjectController {
+  constructor(private readonly projectService: ProjectService) {}
+ /* @Post("")
+  async create(@Body() createScheduleDto: CreateScheduleDto) {
+    return this.scheduleService.create(createScheduleDto);
+  }
+  @Get("")
+  async findAll(@Request() req){
+    return this.scheduleService.all(req.user.id);
+  }*/
+/*
+
+  @Get()
+  async findAll() {
+    return this.articlesService.findAll();
+  }
+
+  @Get(':id')
+  async findOne(@Param('id') id: string) {
+    return this.articlesService.findOne(id);
+  }
+
+  @Delete(':id')
+  async deleteArticle(@Param('id') id: string) {
+    // tslint:disable-next-line: no-console
+    console.log('delete article with id -> ', id);
+    return this.articlesService.delete(id);
+  }*/
+  }
