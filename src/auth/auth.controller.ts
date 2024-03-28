443ba2070f44357f941ba63457e908f0f1061fef
AbdElrhman ElHodaky <47593167+abdoElHodaky@users.noreply.github.com>
2024-03-28 15:26:12 +0200

    Update auth.controller.ts

--- src/auth/auth.controller.ts
+++ src/auth/auth.controller.ts
@@ -2,17 +2,16 @@ import { Controller, Post, Body } from '@nestjs/common';
 import { AuthService } from './auth.service';
 import { LoginUserDto } from '../users/dto/login-user.dto';
 import { CreateUserDto } from '../users/dto/create-user.dto';
-import { ApiTags, ApiExcludeEndpoint } from "@nestjs/swagger";
+import { ApiTags } from "@nestjs/swagger";
 @ApiTags("Auth")
 @Controller('auth')
 export class AuthController {
   constructor(private authService: AuthService) {}
-  //@ApiExcludeEndpoint()
   @Post('login')
   async login(@Body() loginUserDto: LoginUserDto) {
     return this.authService.login(loginUserDto);
   }
-  //@ApiExcludeEndpoint()
+  
   @Post('register')
   async register(@Body() createUserDto: CreateUserDto) {
     return this.authService.register(createUserDto);
