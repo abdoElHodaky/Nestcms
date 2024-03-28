b1b086148589fb74f3789bbdc43fcfb39ad30a35
AbdElrhman ElHodaky <47593167+abdoElHodaky@users.noreply.github.com>
2024-03-28 23:47:51 +0200

    Create contracts.service.ts

---
+++ src/offers/contracts.service.ts
@@ -0,0 +1,32 @@
+import { Injectable } from '@nestjs/common';
+import { Model ,Types} from 'mongoose';
+import { InjectModel } from '@nestjs/mongoose';
+import { CreateOfferDto } from './dto/create-offer.dto';
+import { Offer } from './interface/offer.interface';
+import { UsersService} from "../users/users.service"
+import { ContractService} from "../contracts/contracts.service"
+@Injectable()
+export class ContractService {
+  constructor(@InjectModel('Offer') private readonly offerModel: Model<Offer>) {}
+  private userService:UsersService
+ 
+  async create(createOfferDto: CreateOfferDto): Promise<Contract> {
+    const {clientId,employeeId,...rest}=CreateOfferDto
+    const [client,employee]=await this.userService.findMany_Id([clientId,employeeId])
+    const createdOffer = new this.offerModel(rest);
+    createdOffer.client=client
+    createdOffer.employee=employee
+    return await createdOffer.save();
+  }
+  async all(uid:string):Promise<Contract[]>{
+    const employee = await this.userService.find_Id(uid)
+    return this.offerModel.find().populate({
+      path:"employee",
+      match:{"employee._id":employee._id},
+    }).exec();
+  }
+/*
+  async findOne(email: string): Promise<User> {
+    return await this.userModel.findOne({ email }, '-__v').exec();
+  } */
+}
