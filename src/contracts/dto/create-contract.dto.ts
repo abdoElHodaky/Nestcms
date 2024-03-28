240f2f93ffed1ca04f0290f3e622eab104252a1e
AbdElrhman ElHodaky <47593167+abdoElHodaky@users.noreply.github.com>
2024-03-28 20:42:56 +0200

    Update create-contract.dto.ts

--- src/contracts/dto/create-contract.dto.ts
+++ src/contracts/dto/create-contract.dto.ts
@@ -1,11 +1,19 @@
+import { ApiProperty, ApiPropertyOptional } from '@nestjs/swagger';
+
 export class CreateContractDto {
-  
+  @ApiProperty()
   readonly title: string;
+  @ApiProperty()
   readonly content: string;
+  @ApiPropertyOptional()
   readonly author: string;
+  @ApiProperty()
   readonly startDate: string;
+  @ApiProperty()
   readonly endDate:string;
+  @ApiProperty()
   readonly status:string;
+  @ApiPropertyOptional()
   readonly path?:string
   //readonly employeeId?:string;
 
