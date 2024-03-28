d368ebfd208a7e68f63d06f17348b36cc7192bd1
AbdElrhman ElHodaky <47593167+abdoElHodaky@users.noreply.github.com>
2024-03-28 08:59:57 +0200

    Update routes.ts

--- src/routes.ts
+++ src/routes.ts
@@ -1,1 +1,28 @@
+import { SchedulesModule } from "./schedules/schedules.module";
+import { ProjectsModule } from "./projects/projects.module";
+import { AuthModule } from "./auth/auth.module";
 
+export const apiroutes= [{
+    path: 'api',
+   // module: NinjaModule,
+    children: [
+        {
+            path: '/schedules',
+            module: SchedulesModule,
+        },
+        {
+            path: '/projects',
+            module: ProjectsModule,
+        },
+    ],
+}];
+export const routes= [{
+    path: '',
+   // module: NinjaModule,
+    children: [
+        {
+            path: '/auth',
+            module: SchedulesModule,
+        },
+    ],
+}];
