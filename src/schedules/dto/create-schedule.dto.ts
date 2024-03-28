ce14d0b5e8d6386e23ef5ab7cf9a4d21b3e807dc
AbdElrhman ElHodaky <47593167+abdoElHodaky@users.noreply.github.com>
2024-03-28 10:32:46 +0200

    Update create-schedule.dto.ts

--- src/schedules/dto/create-schedule.dto.ts
+++ src/schedules/dto/create-schedule.dto.ts
@@ -1,9 +1,31 @@
+import { ApiProperty, ApiPropertyOptional } from '@nestjs/swagger';
+
+
 export class CreateScheduleDto {
-  
+   @ApiProperty({
+    type: String,
+    description: 'This is a required property',
+  })
   readonly title: string;
+   @ApiProperty({
+    type: String,
+    description: 'This is a required property',
+  })
   readonly content: string;
+   @ApiProperty({
+    type: String,
+    description: 'This is a required property',
+  })
   readonly startDate: string;
+   @ApiProperty({
+    type: String,
+    description: 'This is a required property',
+  })
   readonly endDate:string;
+   @ApiPropertyOptial({
+    type: String,
+    description: 'This is a required property',
+  })
   readonly employeeId?:string;
   readonly clientId?:string
 }
