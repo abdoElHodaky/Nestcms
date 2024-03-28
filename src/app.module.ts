b0f2124be508a86f271469e72f924c3709022307
AbdElrhman ElHodaky <47593167+abdoElHodaky@users.noreply.github.com>
2024-03-28 09:21:52 +0200

    Update app.module.ts

--- src/app.module.ts
+++ src/app.module.ts
@@ -1,5 +1,5 @@
 import { Module, NestModule, MiddlewareConsumer } from '@nestjs/common';
-import { RouteModule} from "@nestjs/core";
+import { RouterModule} from "@nestjs/core";
 import { MongooseModule } from '@nestjs/mongoose';
 import { AppController } from './app.controller';
 import { AppService } from './app.service';
