d1ce9d1a5b86b45a097953089ea948d5e28ef6bc
AbdElrhman ElHodaky <47593167+abdoElHodaky@users.noreply.github.com>
2024-03-28 10:52:02 +0200

    Update login-user.dto.ts

--- src/users/dto/login-user.dto.ts
+++ src/users/dto/login-user.dto.ts
@@ -1,12 +1,12 @@
 import { ApiProperty, ApiPropertyOptional } from '@nestjs/swagger';
 export class LoginUserDto {
   @ApiProperty({
-    type: String,
+    
     description: 'This is a required property',
   })
   readonly email: string;
   @ApiProperty({
-    type: String,
+    
     description: 'This is a required property',
   })
   readonly password: string;
