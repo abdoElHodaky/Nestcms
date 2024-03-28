728ec9f0c74c27ff001c48c927f072ab7439e455
AbdElrhman ElHodaky <47593167+abdoElHodaky@users.noreply.github.com>
2024-03-28 23:52:35 +0200

    Update offer.interface.ts

--- src/offers/interface/offer.interface.ts
+++ src/offers/interface/offer.interface.ts
@@ -1,1 +1,11 @@
-
+export class Offer {
+  title: string;
+  content: string;
+  creationDate: string;
+  endDate:string;
+  status:string;
+  price:string;
+  contract?:{};
+  client?:{};
+  employee?:{};
+}
