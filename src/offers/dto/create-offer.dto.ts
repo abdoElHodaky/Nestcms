dd64670d664e69cbfdca15ebd6ec133d59928d9c
AbdElrhman ElHodaky <47593167+abdoElHodaky@users.noreply.github.com>
2024-03-28 23:54:53 +0200

    Update create-offer.dto.ts

--- src/offers/dto/create-offer.dto.ts
+++ src/offers/dto/create-offer.dto.ts
@@ -1,1 +1,17 @@
+import { ApiProperty, ApiPropertyOptional } from '@nestjs/swagger';
 
+export class CreateOfferDto {
+  
+  @ApiProperty()
+  readonly title: string;
+  @ApiProperty()
+  readonly content: string;
+  @ApiProperty()
+  readonly startDate: string;
+  @ApiProperty()
+  readonly endDate:string;
+  @ApiProperty()
+  readonly status:string;
+  @ApiPropertyOptional()
+  readonly employeeId?:string;
+}
