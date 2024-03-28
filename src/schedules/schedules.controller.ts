25acba58507f8b04f6c1698b6b419de1362f1668
AbdElrhman ElHodaky <47593167+abdoElHodaky@users.noreply.github.com>
2024-03-28 14:11:55 +0200

    Update schedules.controller.ts

--- src/schedules/schedules.controller.ts
+++ src/schedules/schedules.controller.ts
@@ -2,10 +2,11 @@ import { Controller, Post, Body, Get, Delete, Param, UseInterceptors, Put,UseGua
 import { AuthGuard } from '@nestjs/passport';
 import { CreateScheduleDto } from './dto/create-schedule.dto';
 import { ScheduleService } from './schedules.service';
-import { ApiTags } from "@nestjs/swagger";
+import { ApiTags,ApiSecurity } from "@nestjs/swagger";
 //import { CurrentUserInterceptor } from '../currentuser.interceptor';
 //import { UpdateArticleDto } from './dto/update-article.dto';
 //@UseInterceptors(CurrentUserInterceptor)
+@ApiSecurity("bearer")
 @ApiTags("Schedule")
 @UseGuards(AuthGuard('jwt'))
 @Controller("api/schedules")
