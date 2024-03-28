97a4e937aaa069181259aa4cd7f6e66d73f838e6
AbdElrhman ElHodaky <47593167+abdoElHodaky@users.noreply.github.com>
2024-03-28 14:39:37 +0200

    Update users.controller.ts

--- src/users/users.controller.ts
+++ src/users/users.controller.ts
@@ -1,7 +1,8 @@
 import { Controller, Post, Body } from '@nestjs/common';
 import { UsersService } from './users.service';
 import { CreateUserDto } from './dto/create-user.dto';
-
+import { ApiExcludeController } from "@nestjs/swagger";
+@ApiExcludeController()
 @Controller('users')
 export class UsersController {
   constructor(private readonly usersService: UsersService) {}
