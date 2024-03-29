22332b896a933608483e9c3819f28961dabb8f5e
AbdElrhman ElHodaky <47593167+abdoElHodaky@users.noreply.github.com>
2024-03-29 15:23:39 +0200

    Update contracts.module.ts

--- src/contracts/contracts.module.ts
+++ src/contracts/contracts.module.ts
@@ -1,1 +1,12 @@
-
+import { Module } from '@nestjs/common';
+import { ContractService } from './contracts.service';
+import { ProjectController } from './contracts.controller';
+import { MongooseModule } from '@nestjs/mongoose';
+import { ContractSchema } from './models/contract.schema';
+@Module({
+  imports: [MongooseModule.forFeature([{ name: 'Project', schema: ProjectSchema },{name: 'Design', schema: DesignSchema}])],
+  providers: [ContractService],
+  exports: [ContractService],
+  controllers: [ContractController],
+})
+export class ContractsModule {}
