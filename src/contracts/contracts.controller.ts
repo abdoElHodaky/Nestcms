6c85ece8681093f846c3c09ae2f67ec7aa5c36f4
AbdElrhman ElHodaky <47593167+abdoElHodaky@users.noreply.github.com>
2024-03-29 15:27:37 +0200

    Update contracts.controller.ts

--- src/contracts/contracts.controller.ts
+++ src/contracts/contracts.controller.ts
@@ -1,1 +1,41 @@
+import { Controller, Post, Body, Get, Delete, Param, UseInterceptors, Put,UseGuards,Request } from '@nestjs/common';
+import { AuthGuard } from '@nestjs/passport';
+import { CreateContractDto } from './dto/create-contract.dto';
+import { ContractService } from './contracts.service';
+import { ApiTags,ApiSecurity,ApiBearerAuth } from "@nestjs/swagger";
+//import { CurrentUserInterceptor } from '../currentuser.interceptor';
+//import { UpdateArticleDto } from './dto/update-article.dto';
+//@UseInterceptors(CurrentUserInterceptor)
+@ApiBearerAuth('JWTAuthorization')
+@ApiTags("Contract")
+@UseGuards(AuthGuard('jwt'))
+@Controller("api/contracts")
+export class ContractController {
+  constructor(private readonly contractService: ContractService) {}
+  @Post("create")
+  async create(@Body() createContractDto: CreateContractDto) {
+    return await this.contractService.create(createScheduleDto);
+  }
+  @Get("")
+  async findAll(@Request() req){
+    return await this.contractService.all(req.user.id);
+  }
+/*
 
+  @Get()
+  async findAll() {
+    return this.articlesService.findAll();
+  }
+
+  @Get(':id')
+  async findOne(@Param('id') id: string) {
+    return this.articlesService.findOne(id);
+  }
+
+  @Delete(':id')
+  async deleteArticle(@Param('id') id: string) {
+    // tslint:disable-next-line: no-console
+    console.log('delete article with id -> ', id);
+    return this.articlesService.delete(id);
+  }*/
+}
