c263d3db073d59ed12a34843a313697dd021b77f
AbdElrhman ElHodaky <47593167+abdoElHodaky@users.noreply.github.com>
2024-03-30 20:45:38 +0200

    Update app.module.ts

--- src/app.module.ts
+++ src/app.module.ts
@@ -2,9 +2,9 @@ import { Module, NestModule, MiddlewareConsumer } from '@nestjs/common';
 import { MongooseModule } from '@nestjs/mongoose';
 import { AppController } from './app.controller';
 import { AppService } from './app.service';
-import { ArticlesModule } from './articles/articles.module';
+//import { ArticlesModule } from './articles/articles.module';
 import { AuthModule } from './auth/auth.module';
-import { UsersModule } from './users/users.module';
+//import { UsersModule } from './users/users.module';
 import { SchedulesModule } from './schedules/schedules.module';
 import { ProjectsModule } from './projects/projects.module';
 import { ContractsModule } from './contracts/contracts.module';
@@ -14,7 +14,7 @@ import { ContractsModule } from './contracts/contracts.module';
     MongooseModule.forRoot("mongodb+srv://abdoarh36:TyWF4ABOefQhJFbP@cluster0.bc7sxu7.mongodb.net/nestcms", { useNewUrlParser: true, useFindAndModify: false }),
     //ArticlesModule,
       AuthModule,
-      UsersModule, 
+      //UsersModule, 
      SchedulesModule,
      ProjectsModule,
      //ContractsModule,
