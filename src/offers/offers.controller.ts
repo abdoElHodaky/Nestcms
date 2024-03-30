9491cd8976853cfa909588d5d8991b50bfdb5631
AbdElrhman ElHodaky <47593167+abdoElHodaky@users.noreply.github.com>
2024-03-30 22:55:39 +0200

    Update offers.controller.ts

--- src/offers/offers.controller.ts
+++ src/offers/offers.controller.ts
@@ -1,1 +1,41 @@
+import { Controller, Post, Body, Get, Delete, Param, UseInterceptors, Put,UseGuards,Request } from '@nestjs/common';
+import { AuthGuard } from '@nestjs/passport';
+import { CreateOfferDto } from './dto/create-offer.dto';
+import { OfferService } from './offers.service';
+import { ApiTags,ApiSecurity,ApiBearerAuth } from "@nestjs/swagger";
+//import { CurrentUserInterceptor } from '../currentuser.interceptor';
+//import { UpdateArticleDto } from './dto/update-article.dto';
+//@UseInterceptors(CurrentUserInterceptor)
+@ApiBearerAuth('JWTAuthorization')
+@ApiTags("Offer")
+@UseGuards(AuthGuard('jwt'))
+@Controller("api/offers")
+export class OfferController {
+  constructor(private readonly offerService: OfferService) {}
+  @Post("create")
+  async create(@Body() createOfferDto: CreateOfferDto) {
+    return await this.offerService.create(createOfferDto);
+  }
+  @Get("employee")
+  async employee_all(@Request() req){
+    return await this.offerService.employee_all(req.user.id);
+  }
+/*
 
+  @Get()
+  async findAll() {
+    return this.articlesService.findAll();
+  }
+
+  @Get(':id')
+  async findOne(@Param('id') id: string) {
+    return this.articlesService.findOne(id);
+  }
+
+  @Delete(':id')
+  async deleteArticle(@Param('id') id: string) {
+    // tslint:disable-next-line: no-console
+    console.log('delete article with id -> ', id);
+    return this.articlesService.delete(id);
+  }*/
+}
