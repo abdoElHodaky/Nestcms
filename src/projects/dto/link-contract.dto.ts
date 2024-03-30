df06e5dbedaffc634a8e8d9b333ab0d1756b2932
AbdElrhman ElHodaky <47593167+abdoElHodaky@users.noreply.github.com>
2024-03-30 10:41:33 +0200

    Update link-contract.dto.ts

--- src/projects/dto/link-contract.dto.ts
+++ src/projects/dto/link-contract.dto.ts
@@ -1,6 +1,6 @@
 import { ApiProperty, ApiPropertyOptional } from '@nestjs/swagger';
 
-export class LinktoToContractDto {
+export class LinkToContractDto {
   @ApiProperty()
   readonly projectId: string;
   @ApiProperty()
