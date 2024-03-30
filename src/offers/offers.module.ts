410ccb6bed025390c30075a83255ffdb97cccf5e
AbdElrhman ElHodaky <47593167+abdoElHodaky@users.noreply.github.com>
2024-03-30 22:52:35 +0200

    Update offers.module.ts

--- src/offers/offers.module.ts
+++ src/offers/offers.module.ts
@@ -1,1 +1,12 @@
-
+import { Module } from '@nestjs/common';
+import { OfferService } from './offers.service';
+import { OfferController } from './offers.controller';
+import { MongooseModule } from '@nestjs/mongoose';
+import { OfferSchema } from './models/offer.schema';
+@Module({
+  imports: [MongooseModule.forFeature([{ name: 'Offer', schema: OfferSchema }])],
+  providers: [OfferService],
+  exports: [OfferService],
+  controllers: [OfferController],
+})
+export class OffersModule {}
