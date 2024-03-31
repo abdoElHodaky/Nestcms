69aa7f08541d7aeb7567ba4b254a5759fd935ee4
AbdElrhman ElHodaky <47593167+abdoElHodaky@users.noreply.github.com>
2024-03-31 18:36:54 +0200

    Update permission.schema.ts

--- src/permissions/models/permission.schema.ts
+++ src/permissions/models/permission.schema.ts
@@ -1,1 +1,15 @@
+import * as mongoose from 'mongoose';
 
+export const PermissionSchema = new mongoose.Schema({
+  on: {
+    type: mongoose.Schema.Types.ObjectId,
+    required: true,
+    refPath: 'onModel'
+  },
+  onModel: {
+    type: String,
+    required: true,
+    enum: ['Project', 'ProjectSchedule']
+  }
+
+},{timestamps:true});
