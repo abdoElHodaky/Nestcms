3b575555829c1e16883b25e10b937cb13c7bbc7c
AbdElrhman ElHodaky <47593167+abdoElHodaky@users.noreply.github.com>
2024-03-31 17:46:34 +0200

    Update project-step.interface.ts

--- src/projects/interface/project-step.interface.ts
+++ src/projects/interface/project-step.interface.ts
@@ -1,1 +1,10 @@
-
+import { Project } from "./project.interface";
+//import { Contract } from "../contracts/contract.interface";
+export interface ProjectStep {
+  _id: string;
+  startDate:string;
+  endDate:string;
+  content:string;
+  status:string;
+  project?:Project
+}
