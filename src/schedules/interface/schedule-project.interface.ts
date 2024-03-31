931281be9298857383dd89d395dd264990784419
AbdElrhman ElHodaky <47593167+abdoElHodaky@users.noreply.github.com>
2024-03-31 17:43:14 +0200

    Update schedule-project.interface.ts

--- src/schedules/interface/schedule-project.interface.ts
+++ src/schedules/interface/schedule-project.interface.ts
@@ -1,1 +1,15 @@
+import { Project } from "../projects/interface/project.interface";
+import { ProjectStep } from "../projects/interface/project-step.interface";
 
+export class ScheduleProject{
+ _id:string;
+ title: string;
+  content: string;
+  author: string;
+  creationDate: string;
+  endingDate:string;
+  status:string;
+  project?:Project;
+  projectSteps?:ProjectStep[]
+  
+}
