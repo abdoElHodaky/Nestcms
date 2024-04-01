5baef1690223cb6b6d6d38e66f76c239b397ef8f
AbdElrhman ElHodaky <47593167+abdoElHodaky@users.noreply.github.com>
2024-04-01 12:05:09 +0200

    Update users.service.ts

--- src/users/users.service.ts
+++ src/users/users.service.ts
@@ -31,7 +31,7 @@ export class UsersService {
       }
     ]).exec()*/
     const userData = await this.userModel.aggregate([
-            { $match: { username: Types.ObjectId(_id) } },
+            { $match: { _id: Types.ObjectId(_id) } },
             {
                 $lookup: {
                     from: "permissions",
