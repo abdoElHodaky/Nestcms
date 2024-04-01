b249acd49e0049d9568b1a34d6440f467461bb6b
AbdElrhman ElHodaky <47593167+abdoElHodaky@users.noreply.github.com>
2024-04-01 16:30:15 +0200

    Update create-project-schedule.dto.ts

--- src/schedules/dto/create-project-schedule.dto.ts
+++ src/schedules/dto/create-project-schedule.dto.ts
@@ -1,7 +1,7 @@
 import { ApiProperty, ApiPropertyOptional } from '@nestjs/swagger';
 
 
-export class CreateScheduleDto {
+export class CreateProjectScheduleDto {
    @ApiProperty()
   readonly title: string;
    @ApiProperty()
