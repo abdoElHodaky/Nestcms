6e19fd78cdf1cd280fad56a9036483fd8de725e8
AbdElrhman ElHodaky <47593167+abdoElHodaky@users.noreply.github.com>
2024-04-01 13:26:38 +0200

    Update app.module.ts

--- src/app.module.ts
+++ src/app.module.ts
@@ -4,7 +4,7 @@ import { AppController } from './app.controller';
 import { AppService } from './app.service';
 //import { ArticlesModule } from './articles/articles.module';
 import { AuthModule } from './auth/auth.module';
-//import { UsersModule } from './users/users.module';
+import { UsersModule } from './users/users.module';
 import { SchedulesModule } from './schedules/schedules.module';
 import { ProjectsModule } from './projects/projects.module';
 import { ContractsModule } from './contracts/contracts.module';
