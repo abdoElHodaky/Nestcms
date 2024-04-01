1a5bac1361a211b8ccc6c4a12bea9cac56d37763
AbdElrhman ElHodaky <47593167+abdoElHodaky@users.noreply.github.com>
2024-04-01 16:03:59 +0200

    Update create-project-step.dto.ts

--- src/projects/dto/create-project-step.dto.ts
+++ src/projects/dto/create-project-step.dto.ts
@@ -1,1 +1,14 @@
+import { ApiProperty, ApiPropertyOptional } from '@nestjs/swagger';
 
+
+export class CreateProjectStepDto {
+  @ApiProperty()
+  readonly desc:string;
+  @ApiProperty()
+  readonly projectId:string;
+  @ApiPorperty()
+  startDate:string;
+  @ApiProperty()
+  endDate:string;
+  
+}
