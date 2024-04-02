f637f8caa2cd2a4d9ef68d4d67294589749ef1f2
AbdElrhman ElHodaky <47593167+abdoElHodaky@users.noreply.github.com>
2024-04-02 16:51:19 +0200

    Update schedules.controller.ts

--- src/schedules/schedules.controller.ts
+++ src/schedules/schedules.controller.ts
@@ -9,7 +9,7 @@ import { ApiTags,ApiSecurity,ApiBearerAuth } from "@nestjs/swagger";
 @ApiBearerAuth('JWTAuthorization')
 @ApiTags("Schedule")
 @UseGuards(AuthGuard('jwt'))
-@Controller("api/schedules")
+@Controller("schedules")
 export class ScheduleController {
   constructor(private readonly scheduleService: ScheduleService) {}
   @Post("create")
