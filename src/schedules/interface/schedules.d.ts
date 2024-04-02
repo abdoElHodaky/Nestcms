136c0d4fd0b02a3fa73e352a2f38690846643a51
AbdElrhman ElHodaky <47593167+abdoElHodaky@users.noreply.github.com>
2024-04-02 13:33:35 +0200

    Update schedules.d.ts

--- src/schedules/interface/schedules.d.ts
+++ src/schedules/interface/schedules.d.ts
@@ -1,2 +1,20 @@
-declare interface Schedule {}
-declare interface ScheduleProject{}
+declare interface _Schedule {
+  _id:string;
+ title: string;
+  content: string;
+  author: string;
+  creationDate: string;
+  endingDate:string;
+  status:string;
+}
+declare interface _ScheduleProject{
+  _id:string;
+ title: string;
+  content: string;
+  author: string;
+  startingDate: string;
+  endingDate:string;
+  status:string;
+}
+export default _Schedule
+export default _ScheduleProject
