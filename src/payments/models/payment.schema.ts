a3e381eb83d2e3d93a1e86588074d1a2e37c3563
AbdElrhman ElHodaky <47593167+abdoElHodaky@users.noreply.github.com>
2024-04-02 10:29:00 +0200

    Update payment.schema.ts

--- src/payments/models/payment.schema.ts
+++ src/payments/models/payment.schema.ts
@@ -1,1 +1,10 @@
+import * as mongoose from 'mongoose';
 
+export const PaymentSchema = new mongoose.Schema({
+  title: String,
+  content: String,
+  Date: String,
+  status:String,
+  client: { type: mongoose.Schema.Types.ObjectId, ref: 'User'},
+  contract: { type: mongoose.Schema.Types.ObjectId, ref: 'Contract'}
+},{timestamps:true});
