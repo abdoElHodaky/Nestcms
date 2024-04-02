7cbfc48c513d495ddcbe8b9de5f24f7f28405c5a
AbdElrhman ElHodaky <47593167+abdoElHodaky@users.noreply.github.com>
2024-04-02 14:15:04 +0200

    Update project.interface.ts

--- src/projects/interface/project.interface.ts
+++ src/projects/interface/project.interface.ts
@@ -1,7 +1,7 @@
 import { Design } from "./design.interface";
 import { ProjectStep } from "./project-step.interface";
 import { Contract } from "../contracts/contract.interface";
-import _Project from "project.d"
+import _Project from "./project.d"
 export class Project implements _Project {
   _id: string;
   startDate:string;
