f4760b66d18d2bc01d308acd88bac7591e406b57
AbdElrhman ElHodaky <47593167+abdoElHodaky@users.noreply.github.com>
2024-04-02 14:51:24 +0200

    Update project.ts

--- src/projects/interface/project.ts
+++ src/projects/interface/project.ts
@@ -1,6 +1,6 @@
 import { Design } from "./design";
 import { ProjectStep } from "./project-step";
-import { Contract } from "../contracts/interface/contract";
+import { Contract } from "../../contracts/interface/contract";
 import _Project from "./project.d"
 export class Project implements _Project {
   _id: string;
