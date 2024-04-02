28118ae399ceedb484028ac2662dbff48b94df12
AbdElrhman ElHodaky <47593167+abdoElHodaky@users.noreply.github.com>
2024-04-02 19:38:14 +0200

    Update projects.service.ts

--- src/projects/projects.service.ts
+++ src/projects/projects.service.ts
@@ -73,7 +73,7 @@ export class ProjectService {
   }
   async employees(projectId:string):Promise<Employee>{
     const project = await this.projectModel.findById(projectId).exec()
-    return this.userService.find_Id(project.employee._id)
+    return this.userService.find_Id(project.employee)
   }
 /*
   async findOne(email: string): Promise<User> {
