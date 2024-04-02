ea73992d4ac125129e2e2cd89ae4459be029a522
AbdElrhman ElHodaky <47593167+abdoElHodaky@users.noreply.github.com>
2024-04-02 21:53:10 +0200

    Update offer.interface.ts

--- src/offers/interface/offer.interface.ts
+++ src/offers/interface/offer.interface.ts
@@ -1,3 +1,5 @@
+import { Client,Employee } from "../../users/interface/user";
+import { Contract } from "../../contracts/interface/contract";
 export interface Offer {
   title: string;
   content: string;
@@ -5,7 +7,7 @@ export interface Offer {
   endDate:string;
   status:string;
   price:string;
-  contract?:{};
-  client?:{};
-  employee?:{};
+  contract?:Contract;
+  client?:Client;
+  employee?:Employee;
 }
