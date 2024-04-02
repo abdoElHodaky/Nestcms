2864f1114e3d4b2c37ec349ee028dc2d5efcee7e
AbdElrhman ElHodaky <47593167+abdoElHodaky@users.noreply.github.com>
2024-04-02 20:19:40 +0200

    Update project.ts

--- src/projects/interface/project.ts
+++ src/projects/interface/project.ts
@@ -1,6 +1,7 @@
 import { Design } from "./design";
 import { ProjectStep } from "./project-step";
 import { Contract } from "../../contracts/interface/contract";
+import { Employee } from "../../users/interfaces/user";
 import _Project from "./project.d"
 export class Project implements _Project {
   _id: string;
@@ -8,7 +9,7 @@ export class Project implements _Project {
   endDate:string;
   content:string;
   status:string;
-  employee?:{};
+  employee?:Employee;
   designs?:Design[];
   contract?:Contract;
   steps?:ProjectStep[];
