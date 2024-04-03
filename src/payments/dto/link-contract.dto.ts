76273fc89ea224f17d360107af65ab99950d179d
AbdElrhman ElHodaky <47593167+abdoElHodaky@users.noreply.github.com>
2024-04-03 11:16:54 +0200

    Update link-contract.dto.ts

--- src/payments/dto/link-contract.dto.ts
+++ src/payments/dto/link-contract.dto.ts
@@ -1,1 +1,9 @@
+import { ApiProperty, ApiPropertyOptional } from '@nestjs/swagger';
 
+export class PaymentLinkToContractDto {
+  @ApiProperty()
+  readonly paymentId: string;
+  @ApiProperty()
+  readonly contractId: string;
+  
+}
