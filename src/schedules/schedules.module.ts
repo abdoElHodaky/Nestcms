db183024cbcb60cea463e51367e1c9ec9ce190fd
AbdElrhman ElHodaky <47593167+abdoElHodaky@users.noreply.github.com>
2024-04-03 21:05:35 +0200

    Update schedules.module.ts

--- src/schedules/schedules.module.ts
+++ src/schedules/schedules.module.ts
@@ -2,7 +2,7 @@ import { Module } from '@nestjs/common';
 import { ScheduleService } from './schedules.service';
 import { ScheduleProjectService } from "./schedules-projects.service";
 import { ScheduleController } from './schedules.controller';
-import { ScheduleProjectController } from "./schedules-projects.controller ";
+import { ScheduleProjectController } from "./schedules-projects.controller";
 import { MongooseModule } from '@nestjs/mongoose';
 import { ScheduleSchema } from './models/schedule.schema';
 import { ScheduleProjectSchema } from "./models/schedule-project.schema";
