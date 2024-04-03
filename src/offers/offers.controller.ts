734584cac2d85ba1a4ff3e1e0020ed42ebfc8065
AbdElrhman ElHodaky <47593167+abdoElHodaky@users.noreply.github.com>
2024-04-03 10:46:58 +0200

    Update offers.controller.ts

--- src/offers/offers.controller.ts
+++ src/offers/offers.controller.ts
@@ -2,7 +2,7 @@ import { Controller, Post, Body, Get, Delete, Param, UseInterceptors, Put,UseGua
 import { AuthGuard } from '@nestjs/passport';
 import { CreateOfferDto } from './dto/create-offer.dto';
 import { AcceptOfferDto } from './dto/accept-offer.dto';
-import { LinkToContract } from "./dto/link-contract.dto";
+import { LinkToContractDto } from "./dto/link-contract.dto";
 import { OfferService } from './offers.service';
 import { ApiTags,ApiSecurity,ApiBearerAuth } from "@nestjs/swagger";
 //import { CurrentUserInterceptor } from '../currentuser.interceptor';
@@ -27,8 +27,8 @@ export class OfferController {
     return await this.offerService.accept(acceptOfferDto)
   }
   @Post("linkcontract")
-  async linkContract(@Body() linkToContract:LinkToContract){
-    return await this.offerService.LinkContract(linkToContract);
+  async linkContract(@Body() linkToContractDto:LinkToContractDto){
+    return await this.offerService.LinkContract(linkToContractDto);
   }
 /*
 
