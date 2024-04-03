c1aea3e8c74389c65a63035c27299e2ba90a9107
AbdElrhman ElHodaky <47593167+abdoElHodaky@users.noreply.github.com>
2024-04-03 20:48:40 +0200

    Update schedules.module.ts

--- src/schedules/schedules.module.ts
+++ src/schedules/schedules.module.ts
@@ -1,13 +1,15 @@
 import { Module } from '@nestjs/common';
 import { ScheduleService } from './schedules.service';
+import { ScheduleProjectService } from "./schedules-projects.service";
 import { ScheduleController } from './schedules.controller';
+import { ScheduleProjectController } from "./schedules-projects.controller ";
 import { MongooseModule } from '@nestjs/mongoose';
 import { ScheduleSchema } from './models/schedule.schema';
-
+import { ScheduleProjectSchema } from "./models/schedule-project.schema";
 @Module({
-  imports: [MongooseModule.forFeature([{ name: 'Schedule', schema: ScheduleSchema }])],
-  providers: [ScheduleService],
-  exports: [ScheduleService],
-  controllers: [ScheduleController],
+  imports: [MongooseModule.forFeature([{ name: 'Schedule', schema: ScheduleSchema },{name:"ScheduleProject",schema:ScheduleProjectSchema}])],
+  providers: [ScheduleService,ScheduleProjectService ],
+  exports: [ScheduleService,ScheduleProjectService ],
+  controllers: [ScheduleController,ScheduleProjectController ],
 })
 export class SchedulesModule {}
