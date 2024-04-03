165394a134bcf8faf060508b9e734392aa287a1a
AbdElrhman ElHodaky <47593167+abdoElHodaky@users.noreply.github.com>
2024-04-03 21:47:52 +0200

    Update create-note.dto.ts

--- src/notes/dto/create-note.dto.ts
+++ src/notes/dto/create-note.dto.ts
@@ -1,1 +1,16 @@
+import { ApiProperty, ApiPropertyOptional } from '@nestjs/swagger';
 
+export class CreateNoteDto {
+  
+  @ApiProperty()
+  readonly title: string;
+  @ApiProperty()
+  readonly content: string;
+  @ApiProperty()
+  readonly status:string;
+  @ApiProperty()
+  readonly onId:string
+  @ApiProperty()
+  readonly onType:string
+
+}
