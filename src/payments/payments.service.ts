fbda3d31d8344cd9a2369691fae9296666551802
AbdElrhman ElHodaky <47593167+abdoElHodaky@users.noreply.github.com>
2024-04-03 11:27:26 +0200

    Create payments.service.ts

---
+++ src/payments/payments.service.ts
@@ -0,0 +1,52 @@
+import { Injectable } from '@nestjs/common';
+import { Model ,Types} from 'mongoose';
+import { InjectModel } from '@nestjs/mongoose';
+import { CreatePaymentDto } from './dto/create-offer.dto';
+//import { AcceptOfferDto } from './dto/accept-offer.dto';
+import { PaymentLinkToContractDto } from "./dto/link-contract.dto";
+import { Payment } from './interface/payment.interface';
+import { UsersService} from "../users/users.service"
+import { ContractService} from "../contracts/contracts.service"
+@Injectable()
+export class OfferService {
+  constructor(@InjectModel('Payment') private readonly paymModel: Model<Payment>) {}
+  private userService:UsersService
+  private contractService:ContractService
+ 
+ /* async create(createPaymentDto: CreatePaymentDto): Promise<Offer> {
+    const {employeeId,...rest}=createPaymentDto
+    const employee=await this.userService.find_Id(employeeId)
+    const createdPayment = new this.offerModel(rest);
+    createdPayment.employee=employee
+    return await createdPayment.save();
+  }
+  async accept(acceptOfferDto:AcceptOfferDto):Promise<Offer>{
+    const {offerId,clientId,status}=acceptOfferDto
+    const client = await this.userService.find_Id(clientId)
+    const offer= await this.offerModel.findById(offerId).exec()
+    offer.status=status
+    offer.client=client
+    return await offer.save()
+  }
+  async employee_all(uid:string):Promise<Offer[]>{
+    const employee = await this.userService.find_Id(uid)
+    return await this.offerModel.find().populate({
+      path:"employee",
+      match:{"employee._id":employee._id},
+    }).exec();
+  }*/
+  async find_Id(_id:string):Promise<Offer>{
+    return await this.paymModel.findById(_id).exec()
+  }
+  async LinkContract(paymentLinkToContractDto:PaymentLinkToContractDto):Promise<Offer>{
+    const {offerId,contractId}=paymentLinkToContractDto
+    const payment=await this.paymModel.findById(offerId)
+    const contract=await this.contractService.find_Id(contractId)
+    payment.contract=contract
+    return await payment.save()
+  }
+/*
+  async findOne(email: string): Promise<User> {
+    return await this.userModel.findOne({ email }, '-__v').exec();
+  } */
+}
