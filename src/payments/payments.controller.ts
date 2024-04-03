00d536582824722e5d6cbf1283d773d4186c23f5
AbdElrhman ElHodaky <47593167+abdoElHodaky@users.noreply.github.com>
2024-04-03 11:37:23 +0200

    Create payments.controller.ts

---
+++ src/payments/payments.controller.ts
@@ -0,0 +1,51 @@
+import { Controller, Post, Body, Get, Delete, Param, UseInterceptors, Put,UseGuards,Request } from '@nestjs/common';
+import { AuthGuard } from '@nestjs/passport';
+import { CreateOfferDto } from './dto/create-offer.dto';
+import { AcceptOfferDto } from './dto/accept-offer.dto';
+import { PaymentLinkToContractDto } from "./dto/link-contract.dto";
+import { PaymentService } from './offers.service';
+import { ApiTags,ApiSecurity,ApiBearerAuth } from "@nestjs/swagger";
+//import { CurrentUserInterceptor } from '../currentuser.interceptor';
+//import { UpdateArticleDto } from './dto/update-article.dto';
+//@UseInterceptors(CurrentUserInterceptor)
+@ApiBearerAuth('JWTAuthorization')
+@ApiTags("Payment")
+@UseGuards(AuthGuard('jwt'))
+@Controller(["api/payments"])
+export class PaymentController {
+  constructor(private readonly paymentService: PaymentService) {}
+/*  @Post("create")
+  async create(@Body() createOfferDto: CreateOfferDto) {
+    return await this.offerService.create(createOfferDto);
+  }
+  @Get("employee")
+  async employee_all(@Request() req){
+    return await this.offerService.employee_all(req.user.id);
+  }
+  @Post("accept")
+  async employee_accept(@Body() acceptOfferDto:AcceptOfferDto){
+    return await this.offerService.accept(acceptOfferDto)
+  }*/
+  @Post("linkcontract")
+  async linkContract(@Body() paymentLinkToContractDto: PaymentLinkToContractDto){
+    return await this.offerService.LinkContract(paymentLinkToContractDto);
+  }
+/*
+
+  @Get()
+  async findAll() {
+    return this.articlesService.findAll();
+  }
+
+  @Get(':id')
+  async findOne(@Param('id') id: string) {
+    return this.articlesService.findOne(id);
+  }
+
+  @Delete(':id')
+  async deleteArticle(@Param('id') id: string) {
+    // tslint:disable-next-line: no-console
+    console.log('delete article with id -> ', id);
+    return this.articlesService.delete(id);
+  }*/
+}
