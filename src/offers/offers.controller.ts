b71d4e2d2b84a7591a2a0f815204b09257d6c7d2
AbdElrhman ElHodaky <47593167+abdoElHodaky@users.noreply.github.com>
2024-04-03 11:05:18 +0200

    Update offers.controller.ts

--- src/offers/offers.controller.ts
+++ src/offers/offers.controller.ts
@@ -11,7 +11,7 @@ import { ApiTags,ApiSecurity,ApiBearerAuth } from "@nestjs/swagger";
 @ApiBearerAuth('JWTAuthorization')
 @ApiTags("Offer")
 @UseGuards(AuthGuard('jwt'))
-@Controller(["api","offers"])
+@Controller(["api/offers"])
 export class OfferController {
   constructor(private readonly offerService: OfferService) {}
   @Post("create")
