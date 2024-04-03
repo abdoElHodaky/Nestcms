364f20e1e3d7c5ea6426339ff0c27829eebcb245
AbdElrhman ElHodaky <47593167+abdoElHodaky@users.noreply.github.com>
2024-04-03 23:20:15 +0200

    Update notes.module.ts

--- src/notes/notes.module.ts
+++ src/notes/notes.module.ts
@@ -1,1 +1,12 @@
-
+import { Module } from '@nestjs/common';
+import { NoteService } from './note.service';
+import { NoteController } from './note.controller';
+import { MongooseModule } from '@nestjs/mongoose';
+import { NoteSchema } from './models/note.schema';
+@Module({
+  imports: [MongooseModule.forFeature([{ name: 'Note', schema: NoteSchema }])],
+  providers: [NoteService],
+  exports: [NoteService],
+  controllers: [NoteController],
+})
+export class NotesModule {}
