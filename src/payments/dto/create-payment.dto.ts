9347b707c597d522aa0f4029bfdb4570eaf8552b
AbdElrhman ElHodaky <47593167+abdoElHodaky@users.noreply.github.com>
2024-04-03 11:47:29 +0200

    Update create-payment.dto.ts

--- src/payments/dto/create-payment.dto.ts
+++ src/payments/dto/create-payment.dto.ts
@@ -1,1 +1,18 @@
+import { ApiProperty, ApiPropertyOptional } from '@nestjs/swagger';
 
+export class CreatePaymentDto {
+  @ApiProperty()
+  readonly title: string;
+  @ApiProperty()
+  readonly content: string;
+  @ApiProperty()
+  readonly Date:string
+  @ApiPropertyOptional()
+  readonly status:string;
+  @ApiProperty()
+  readonly amount:number;
+  @ApiProperty()
+  readonly currency:string;
+ 
+  
+}
