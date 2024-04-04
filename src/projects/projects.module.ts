dff5826086058df86b7e898d0ba4fd849686031b
AbdElrhman ElHodaky <47593167+abdoElHodaky@users.noreply.github.com>
2024-04-04 10:42:31 +0200

    Update projects.module.ts

--- src/projects/projects.module.ts
+++ src/projects/projects.module.ts
@@ -1,4 +1,5 @@
 import { Module } from '@nestjs/common';
+import { RouterModule } from "@nestjs/core";
 import { ProjectService } from './projects.service';
 import { ProjectController } from './projects.controller';
 import { MongooseModule } from '@nestjs/mongoose';
@@ -8,11 +9,14 @@ import { ProjectStepSchema } from './models/project-step.schema';
 import { DesignController } from './designs.controller';
 import { DesignSchema } from './models/design.schema';
 import { NoteController } from './notes.controller';
-
+import { routes} from "./routes";
 @Module({
-  imports: [MongooseModule.forFeature([{ name: 'Project', schema: ProjectSchema },{name: 'Design', schema: DesignSchema}, {name: 'ProjectStep', schema: ProjectStepSchema}])],
+  imports: [MongooseModule.forFeature([{ name: 'Project', schema: ProjectSchema },
+                                       {name: 'Design', schema: DesignSchema},
+                                       {name: 'ProjectStep', schema: ProjectStepSchema}]),
+           RouterModule.register(routes)],
   providers: [ProjectService],
   exports: [ProjectService],
-  controllers: [ProjectController,DesignController,StepController,NoteController],
+  //controllers: [ProjectController,DesignController,StepController,NoteController],
 })
 export class ProjectsModule {}
