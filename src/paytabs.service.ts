51e1c7021412ded414aaca6fac01f3b238d2cb51
AbdElrhman ElHodaky <47593167+abdoElHodaky@users.noreply.github.com>
2024-04-04 21:27:41 +0200

    Update paytabs.service.ts

--- src/paytabs.service.ts
+++ src/paytabs.service.ts
@@ -1,10 +1,14 @@
 import { Injectable } from '@nestjs/common';
 import paytabs from "paytabs_pt2";
+import { Client } from "./users/interfaces/user";
 @Injectable()
 class PayTabsService{
-   
+  async values(obj){
+      return obj.values()
+   }
    async config(profile:string,serverk:string,region:string):Promise<any>
     {
       await paytabs.config(profile,serverk,region)
     }
+   async createPage(){}
 }
