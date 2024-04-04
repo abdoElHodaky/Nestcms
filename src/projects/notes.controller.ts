d1e2ebf7375df4130e12a06a8c8f480d41ca2ca4
AbdElrhman ElHodaky <47593167+abdoElHodaky@users.noreply.github.com>
2024-04-04 10:00:16 +0200

    Update notes.controller.ts

--- src/projects/notes.controller.ts
+++ src/projects/notes.controller.ts
@@ -1,1 +1,44 @@
 
+import { Controller, Post, Body, Get, Delete, Param, UseInterceptors, Put,UseGuards,Request } from '@nestjs/common';
+import { AuthGuard } from '@nestjs/passport';
+import { ProjectService } from "./projects.service";
+import { NoteService } from "../notes/notes.service";
+import { CreateNoteDto } from "../notes/dto/create-note.dto";
+import { ApiTags,ApiSecurity,ApiBearerAuth } from "@nestjs/swagger";
+@ApiBearerAuth('JWTAuthorization')
+//@ApiSecurity("bearer")
+@ApiTags("Project.Note")
+@UseGuards(AuthGuard('jwt'))
+@Controller('api/projects/:id/notes')
+export class NoteController {
+  private noteService:NoteService;
+  constructor(private readonly projectService: ProjectService) {}
+  @Post("add")
+  async create(@Body() createNoteDto: CreateNoteDto) {
+    return this.noteService.create(createNoteDto);
+  }
+  
+  @Get("")
+  async findAll(@Param("id") id:string){
+   // return this.scheduleService.all(req.user.id);
+   return this.projectService.notes(id)
+  }
+  /*
+
+  @Get()
+  async findAll() {
+    return this.articlesService.findAll();
+  }
+
+  @Get(':id')
+  async findOne(@Param('id') id: string) {
+    return this.articlesService.findOne(id);
+  }
+
+  @Delete(':id')
+  async deleteArticle(@Param('id') id: string) {
+    // tslint:disable-next-line: no-console
+    console.log('delete article with id -> ', id);
+    return this.articlesService.delete(id);
+  }*/
+  }
