b2b8a0ed501ff6fb1f3d1c3b93f43f23f9c803fd
AbdElrhman ElHodaky <47593167+abdoElHodaky@users.noreply.github.com>
2024-04-04 22:17:23 +0200

    Update payment.interface.ts

--- src/payments/interface/payment.interface.ts
+++ src/payments/interface/payment.interface.ts
@@ -1,10 +1,15 @@
+import { Client } from "../../users/interfaces/user";
+import { Contract } from "../../contracts/interface/contract";
 export interface Payment {
+  _id:string;
   title: string;
   content?: string;
   date: string;
   status:string;
   amount:string;
   currency:string
-  contract?:{};
-  client?:{};
+  contract?:Contract;
+  client?:Client;
+
+  toArray(){}
 }
