bf6f102f5c862e209b700f217bfbecef1ac38b52
AbdElrhman ElHodaky <47593167+abdoElHodaky@users.noreply.github.com>
2024-04-04 10:39:17 +0200

    Update routes.ts

--- src/projects/routes.ts
+++ src/projects/routes.ts
@@ -3,3 +3,29 @@ import { StepController } from './steps.controller';
 import { DesignController } from './designs.controller';
 import { NoteController } from './notes.controller';
 
+
+export const routes= [
+    
+    {
+    path: 'api/projects',
+    controller:ProjectCintroller,
+    children: [
+        {
+            path: '/:id',
+            children:[
+              {
+                path:"/designs",
+                controller:DesignController
+              },
+              {
+                path:"/steps",
+                controller:StepController
+              },{
+                path:"/notes",
+                controller:NoteController
+              }
+            ]
+        },
+        
+    ],
+}];
