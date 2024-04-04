df2fdb57bc023bd89ec5f1cba4e1b3aa3acb87b7
AbdElrhman ElHodaky <47593167+abdoElHodaky@users.noreply.github.com>
2024-04-04 10:34:44 +0200

    Update routes.ts

--- src/projects/routes.ts
+++ src/projects/routes.ts
@@ -1,1 +1,5 @@
+import { ProjectController } from './projects.controller';
+import { StepController } from './steps.controller';
+import { DesignController } from './designs.controller';
+import { NoteController } from './notes.controller';
 
