401e8de54e406508204bbbd0f445ba57c9f7ae1a
AbdElrhman ElHodaky <47593167+abdoElHodaky@users.noreply.github.com>
2024-04-04 13:30:32 +0200

    Update create-client.dto.ts

--- src/users/dto/create-client.dto.ts
+++ src/users/dto/create-client.dto.ts
@@ -1,18 +1,7 @@
 import { ApiProperty, ApiPropertyOptional } from '@nestjs/swagger';
 import { CreateUserDto } from "./create-user.dto";
 export class CreateClientDto extends CreateUserDto {
-  @ApiProperty()
-   readonly fullname:string
-  @ApiProperty()
-   readonly username:string
-  @ApiProperty()
-   readonly email: string;
-  @ApiProperty()
-   readonly password: string;
-  @ApiPropertyOptional()
-   readonly address?:string
-  @ApiPropertyOptional()
-   readonly phone?:string
+  
   @ApiPropertyOptional()
    readonly isEmployee?:boolean=false
   @ApiPropertyOptional()
