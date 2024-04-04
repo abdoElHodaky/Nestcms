d8fcfcb9db40f1d8258ad4aafff0b978068098b1
AbdElrhman ElHodaky <47593167+abdoElHodaky@users.noreply.github.com>
2024-04-04 12:36:38 +0200

    Update app.controller.ts

--- src/app.controller.ts
+++ src/app.controller.ts
@@ -1,29 +1,5 @@
 import { Controller, Get, UseGuards, Post, Request } from '@nestjs/common';
-import { AuthGuard } from '@nestjs/passport';
-import { AuthService } from './auth/auth.service';
-//import { ApiTags } from "@nestjs/swagger";
 @Controller('api')
 export class AppController {
-  constructor(private readonly authService: AuthService) {}
-
-  
- /* @UseGuards(AuthGuard('local'))
-  @Post('login')
-  async login(@Request() req) {
-    return this.authService.login(req.user);
-  }
-  @UseGuards(AuthGuard('local'))
-  @Post('register')
-  async register(@Request() req) {
-    return this.authService.register(req.user);
-  }
-*/
-  @UseGuards(AuthGuard('jwt'))
-  @Get('me')
-  getProfile(@Request() req) {
-    // As we use the JWT guard on this method,
-    // we need to pass a header containing 'Bearer the_corresponding_jwt_token_passed_on_successful_login'
-    // when making a GET request to http://localhost:3000/api/me
-    return req.user;
-  }
+  constructor() {}
 }
