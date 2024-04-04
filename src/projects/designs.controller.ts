9e97ce79d7271bcb02cec0aad3f558ee2f3de2e3
AbdElrhman ElHodaky <47593167+abdoElHodaky@users.noreply.github.com>
2024-04-04 10:43:46 +0200

    Update designs.controller.ts

--- src/projects/designs.controller.ts
+++ src/projects/designs.controller.ts
@@ -7,7 +7,7 @@ import { ApiTags,ApiSecurity,ApiBearerAuth } from "@nestjs/swagger";
 //@ApiSecurity("bearer")
 @ApiTags("Project.Design")
 @UseGuards(AuthGuard('jwt'))
-@Controller('api/projects/:id/designs')
+@Controller('')
 export class DesignController {
   constructor(private readonly projectService: ProjectService) {}
   @Post("add")
