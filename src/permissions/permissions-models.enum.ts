29b59aca394c0311fc651b45319d720d3d047106
AbdElrhman ElHodaky <47593167+abdoElHodaky@users.noreply.github.com>
2024-04-05 21:31:35 +0200

    Update permissions-models.enum.ts

--- src/permissions/permissions-models.enum.ts
+++ src/permissions/permissions-models.enum.ts
@@ -1,1 +1,13 @@
+export enum Permission {
+    APPROVE = 'Approve',
+    READ = 'Read',
+    WRITE = 'Write',
+    MODIFY="Modify",
+}
 
+export enum onModel {
+  PAYMENT="Payment",
+  PROJECT="Project",
+  SCHEDULE="Schedule",
+  PROJECTSECHEDULE="ProjectSchedule"
+}
