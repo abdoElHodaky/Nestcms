c63bfddb7135327d0075a3cd979a477742fb14ef
AbdElrhman ElHodaky <47593167+abdoElHodaky@users.noreply.github.com>
2024-04-05 10:38:50 +0200

    Update payments.module.ts

--- src/payments/payments.module.ts
+++ src/payments/payments.module.ts
@@ -1,13 +1,13 @@
 import { Module } from '@nestjs/common';
 import { PaymentService } from './payments.service';
-import { PayTabsSerive } from "../paytabs.service";
+import { PayTabSerive } from "../paytabs.service";
 import { PaymentController } from './payments.controller';
 import { MongooseModule } from '@nestjs/mongoose';
 import { PaymentSchema } from './models/payment.schema';
 @Module({
   imports: [MongooseModule.forFeature([{ name: 'Payment', schema: PaymentSchema }])],
-  providers: [PaymentService,PayTabsSerive ],
-  exports: [PaymentService,PayTabsSerive ],
+  providers: [PaymentService,PayTabSerive ],
+  exports: [PaymentService,PayTabSerive ],
   controllers: [PaymentController],
 })
 export class PaymentsModule {}
