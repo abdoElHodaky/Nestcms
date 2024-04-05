42a63dbc9134bb48a5e7f3f5559ebc98cf5f3e0d
AbdElrhman ElHodaky <47593167+abdoElHodaky@users.noreply.github.com>
2024-04-05 18:55:55 +0200

    Update permissions.service.ts

--- src/permissions/permissions.service.ts
+++ src/permissions/permissions.service.ts
@@ -1,1 +1,9 @@
+import { Injectable } from '@nestjs/common';
+import { Model,Types } from 'mongoose';
+import { InjectModel } from '@nestjs/mongoose';
+import { Permission } from './interfaces/permission';
 
+@Injectable()
+export class PermissionService {
+  constructor(@InjectModel('Permission') private readonly permModel: Model<Permission>) {}
+}
