3b8c82f929d30bf83181d0051d2f8ceb6c463498
AbdElrhman ElHodaky <47593167+abdoElHodaky@users.noreply.github.com>
2024-04-05 22:48:12 +0200

    Update perm.guard.ts

--- src/perm.guard.ts
+++ src/perm.guard.ts
@@ -17,6 +17,11 @@ export class PermGuard implements CanActivate {
   async check(permissions,userId){
     let {_perms,_mdls}=permissions
     let _permissions = this.userService.my_Permissions(userId)[0].permissions
+    let res=_permissions.filter(({onModel,type},i)=>{
+      if(_mdls.includes(onModel)==true && _perms.includes(type))
+        return true
+    })
+    if (res.length>0) return true
     
   }
 }
