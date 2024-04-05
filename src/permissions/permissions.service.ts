1ce04df7f7e9c14fd0ab182187e934b1576f94c7
AbdElrhman ElHodaky <47593167+abdoElHodaky@users.noreply.github.com>
2024-04-05 18:58:48 +0200

    Update permissions.service.ts

--- src/permissions/permissions.service.ts
+++ src/permissions/permissions.service.ts
@@ -1,9 +1,17 @@
 import { Injectable } from '@nestjs/common';
 import { Model,Types } from 'mongoose';
 import { InjectModel } from '@nestjs/mongoose';
-import { Permission } from './interfaces/permission';
+import { Permission } from './interface/permission.interface';
 
 @Injectable()
 export class PermissionService {
   constructor(@InjectModel('Permission') private readonly permModel: Model<Permission>) {}
+
+  async find_Id(_id: string): Promise<Permission> {
+    return await this.permModel.findById(_id).exec();
+  }
+
+  async findMany_Id(_ids:string[]):Promise<Permission[]>{
+    return await this.permModel.find().where('_id').in(_ids).exec()
+  }
 }
