c44dba076de461f92c511381f79bfc733ebff9df
AbdElrhman ElHodaky <47593167+abdoElHodaky@users.noreply.github.com>
2024-04-05 22:28:01 +0200

    Update perm.guard.ts

--- src/perm.guard.ts
+++ src/perm.guard.ts
@@ -1,14 +1,16 @@
-import {NestInterceptor,ExecutionContext,CallHandler,Injectable,CanActivate } from '@nestjs/common';
+import {NestInterceptor,ExecutionContext,CallHandler,Injectable,CanActivate,Reflector } from '@nestjs/common';
 import { UsersService } from './users/users.service';
-
+import { Permission } from "./permissions/permissions-models.decorator";
 @Injectable()
 // "implements" guide us how to put together an interceptor
 export class PermGuard implements CanActivate {
-  constructor(private userService: UsersService) {}
+  constructor(private userService: UsersService, private reflector:Reflector ) {}
   // handler refers to the route handler
   canActivate(
     context: ExecutionContext,
   ): boolean | Promise<boolean> | Observable<boolean> {
+    const permissions = this.reflector.getAllAndOverride<string[],string[]>("permissions",[context.getHandler(),context.getClass()])
+    
     const request = context.switchToHttp().getRequest();
     return check(request);
   }
