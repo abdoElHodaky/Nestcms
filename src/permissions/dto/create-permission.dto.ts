40c3ec566859828fc3aa7204f951aa6b266191b9
AbdElrhman ElHodaky <47593167+abdoElHodaky@users.noreply.github.com>
2024-04-06 09:55:44 +0200

    Update create-permission.dto.ts

--- src/permissions/dto/create-permission.dto.ts
+++ src/permissions/dto/create-permission.dto.ts
@@ -1,1 +1,22 @@
+import { ApiProperty, ApiPropertyOptional } from '@nestjs/swagger';
 
+export class CreatePermissionDto {
+  @ApiProperty()
+  readonly type: string;
+   
+   @ApiProperty()
+  readonly status: string;
+  
+   @ApiProperty()
+  readonly endDate:string;
+  
+   @ApiPropertyOptional()
+  readonly _by?:{};
+  
+   @ApiPropertyOptional()
+  readonly _for?:{};
+  
+   @ApiPropertyOptional()
+   readonly on?:{}
+
+}
