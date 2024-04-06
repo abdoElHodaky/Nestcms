71955df823991663d2ebf36ded289a9d6e5a0c8f
AbdElrhman ElHodaky <47593167+abdoElHodaky@users.noreply.github.com>
2024-04-06 11:21:25 +0200

    Update app.module.ts

--- src/app.module.ts
+++ src/app.module.ts
@@ -11,6 +11,8 @@ import { ProjectsModule } from './projects/projects.module';
 import { ContractsModule } from './contracts/contracts.module';
 import { OffersModule } from './offers/offers.module';
 import { NotesModule } from "./notes/notes.module";
+import { PermissionsModule } from "./permissions/permissions.module";
+
 //import { routes } from "./routes";
 @Module({
   imports: [
@@ -24,6 +26,7 @@ import { NotesModule } from "./notes/notes.module";
       ProjectsModule,
       OffersModule,
       ContractsModule,
+      PermissionsModule
   ],
   controllers: [AppController],
   providers: [AppService],
