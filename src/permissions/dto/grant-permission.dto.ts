169013fa09eeaa3d36582e227c6c2afdbe2a4f7b
AbdElrhman ElHodaky <47593167+abdoElHodaky@users.noreply.github.com>
2024-04-06 09:45:45 +0200

    Update grant-permission.dto.ts

--- src/permissions/dto/grant-permission.dto.ts
+++ src/permissions/dto/grant-permission.dto.ts
@@ -1,16 +1,21 @@
 import { ApiProperty, ApiPropertyOptional } from '@nestjs/swagger';
 
 export class GrantPermissionDto {
+   
+   @ApiProperty()
+   readonly permissionId:string
+   
    @ApiProperty()
   readonly type: string;
+   
    @ApiProperty()
-  readonly status: string;
-   @ApiProperty()
+  readonly status: string="granted";
+  /* @ApiProperty()
   readonly endDate:string;
    @ApiPropertyOptional()
   readonly _by?:{};
    @ApiPropertyOptional()
   readonly _for?:{};
    @ApiPropertyOptional()
-   readonly on?:{}
+   readonly on?:{} */
 }
