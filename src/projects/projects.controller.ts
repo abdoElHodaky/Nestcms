5233ee955fb83d0838ba686929da96c5cb66fe53
AbdElrhman ElHodaky <47593167+abdoElHodaky@users.noreply.github.com>
2024-04-06 21:40:55 +0200

    Update projects.controller.ts

--- src/projects/projects.controller.ts
+++ src/projects/projects.controller.ts
@@ -3,23 +3,29 @@ import { AuthGuard } from '@nestjs/passport';
 import { ProjectService } from "./projects.service";
 import { CreateProjectDto } from "./dto/create-project.dto";
 import { ProjectLinkToContractDto } from "./dto/link-contract.dto";
-import { ApiTags,ApiSecurity,ApiBearerAuth } from "@nestjs/swagger";
+import { ApiTags,ApiSecurity,ApiBearerAuth,ApiOperation } from "@nestjs/swagger";
 @ApiBearerAuth('JWTAuthorization')
 @ApiTags("Project")
 @UseGuards(AuthGuard('jwt'))
 @Controller('api/projects')
 export class ProjectController {
   constructor(private readonly projectService: ProjectService) {}
+ 
+  @ApiOperation({description:" add project  "})
   @Post("")
-  async create(@Body() createScheduleDto: CreateProjectDto) {
-   return this.projectService.create(createScheduleDto);
+  async create(@Body() createProjectDto: CreateProjectDto) {
+   return this.projectService.create(createProjectDto);
    
   }
+
+  @ApiOperation({description:" get projects of specific employee  "})
   @Get("")
   async findAll(@Request() req){
    // return this.scheduleService.all(req.user.id);
     return
   }
+  
+  @ApiOperation({description:" link project to contract "})
   @Post("contract")
   async link_contract(@Body() projectLinkToContractDto:ProjectLinkToContractDto){
     return this.projectService.LinkContract(projectLinkToContractDto)
