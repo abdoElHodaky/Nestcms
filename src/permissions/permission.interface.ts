f111933f60aa66b62cffb3b981acfbd2589fef81
AbdElrhman ElHodaky <47593167+abdoElHodaky@users.noreply.github.com>
2024-04-06 10:54:49 +0200

    Update permission.interface.ts

--- src/permissions/permission.interface.ts
+++ src/permissions/permission.interface.ts
@@ -1,3 +1,12 @@
+import { Admin , User } from "../users/interfaces/user";
 export interface Permission {
-  
+  _id:string;
+  type:string;
+  status:string
+  _by:Admin
+  _for:User
+  onId:string;
+  onModel:string
+
 }
+  
