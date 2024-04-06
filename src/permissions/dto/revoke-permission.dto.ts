ecaade096100b1d3d1008fcf0963fa787b2d7873
AbdElrhman ElHodaky <47593167+abdoElHodaky@users.noreply.github.com>
2024-04-06 10:20:44 +0200

    Update revoke-permission.dto.ts

--- src/permissions/dto/revoke-permission.dto.ts
+++ src/permissions/dto/revoke-permission.dto.ts
@@ -1,1 +1,21 @@
+import { ApiProperty, ApiPropertyOptional } from '@nestjs/swagger';
 
+export class RevokePermissionDto {
+   
+   @ApiProperty()
+   readonly permissionId:string
+   
+   @ApiProperty()
+  readonly type: string;
+   
+   @ApiProperty()
+  readonly status: string="revoked";
+  /* @ApiProperty()
+  readonly endDate:string;
+   @ApiPropertyOptional()
+  readonly _by?:{};
+   @ApiPropertyOptional()
+  readonly _for?:{};
+   @ApiPropertyOptional()
+   readonly on?:{} */
+}
