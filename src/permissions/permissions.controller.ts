3722758d3217c8030324cfd69406baff12f1a592
AbdElrhman ElHodaky <47593167+abdoElHodaky@users.noreply.github.com>
2024-04-15 18:01:36 +0200

    Update permissions.controller.ts

--- src/permissions/permissions.controller.ts
+++ src/permissions/permissions.controller.ts
@@ -1,1 +1,69 @@
+import { Controller, Post, Body, Get, Delete, Param, UseInterceptors, Put,UseGuards,Request ,Res } from '@nestjs/common';
+import { AuthGuard } from '@nestjs/passport';
+import { CreatePermissiontDto } from './dto/create-permission.dto';
+import { PermissionService } from './permissions.service';
+import { ApiTags,ApiSecurity,ApiBearerAuth,ApiExcludeEndpoint,ApiOperation } from "@nestjs/swagger";
+//import { CurrentUserInterceptor } from '../currentuser.interceptor';
+//import { UpdateArticleDto } from './dto/update-article.dto';
+//@UseInterceptors(CurrentUserInterceptor)
+//@ApiBearerAuth('JWTAuthorization')
+@ApiTags("Permission")
+//@UseGuards(AuthGuard('jwt'))
+@Controller(["api/permissions"])
+export class PermissionController {
+  constructor(private readonly permissionService: PermissionService) {}
+ 
+  @ApiBearerAuth('JWTAuthorization')
+  @UseGuards(AuthGuard('jwt'))
+  @Get()
+  @ApiOperation({description:"get all permissions"})
+  async All() {
+    return this.permissionService.All();
+  }
 
+  
+  @ApiBearerAuth('JWTAuthorization')
+  @UseGuards(AuthGuard('jwt'))
+  @Post("create")
+  @ApiOperation({description:"create permission"})
+  async create(@Body() createPermissiontDto: CreatePermissiontDto) {
+    return await this.permissionService.create(createPermissiontDto);
+  }
+  
+  
+
+  
+
+
+  /*
+  @Get("employee")
+  async employee_all(@Request() req){
+    return await this.offerService.employee_all(req.user.id);
+  }
+  @Post("accept")
+  async employee_accept(@Body() acceptOfferDto:AcceptOfferDto){
+    return await this.offerService.accept(acceptOfferDto)
+  }*/
+  /*@Post("linkcontract")
+  async linkContract(@Body() paymentLinkToContractDto: PaymentLinkToContractDto){
+    return await this.paymentService.LinkContract(paymentLinkToContractDto);
+  }
+
+
+  @Get()
+  async findAll() {
+    return this.articlesService.findAll();
+  }
+
+  @Get(':id')
+  async findOne(@Param('id') id: string) {
+    return this.articlesService.findOne(id);
+  }
+
+  @Delete(':id')
+  async deleteArticle(@Param('id') id: string) {
+    // tslint:disable-next-line: no-console
+    console.log('delete article with id -> ', id);
+    return this.articlesService.delete(id);
+  }*/
+}
