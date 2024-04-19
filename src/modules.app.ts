d462ff25b3bfa77cd42cf6ad7ca98f346df52d8e
AbdElrhman ElHodaky <47593167+abdoElHodaky@users.noreply.github.com>
2024-04-19 20:55:36 +0200

    Update modules.app.ts

--- src/modules.app.ts
+++ src/modules.app.ts
@@ -1,3 +1,15 @@
+import { PaymentsModule } from './payments/payments.module';
+import { AuthModule } from './auth/auth.module';
+import { UsersModule } from './users/users.module';
+import { SchedulesModule } from './schedules/schedules.module';
+import { ProjectsModule } from './projects/projects.module';
+import { ContractsModule } from './contracts/contracts.module';
+import { OffersModule } from './offers/offers.module';
+import { NotesModule } from "./notes/notes.module";
+import { PermissionsModule } from "./permissions/permissions.module";
 
 
-const modules=[]
+const modules=[PermissionsModule,AuthModule,
+               UsersModule,SchedulesModule,
+               ProjectsModule,OffersModule,
+               NotesModule]
