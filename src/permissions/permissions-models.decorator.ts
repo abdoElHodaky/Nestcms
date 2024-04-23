820f1d8e020903f7cf15de96377a2c792df1ca4b
AbdElrhman ElHodaky <47593167+abdoElHodaky@users.noreply.github.com>
2024-04-23 16:11:08 +0200

    Update permissions-models.decorator.ts

--- src/permissions/permissions-models.decorator.ts
+++ src/permissions/permissions-models.decorator.ts
@@ -1,5 +1,5 @@
 import {SetMetadata} from '@nestjs/common';
 import { Permission , OnModel } from './permissions-models.enum';
 
-export const Permissions = (perms:string[],models:string[]) => SetMetadata('permissions',{_perms:perms,_mdls:models});
+export const Permissions = (args:{perms:string[],models:string[]}) => SetMetadata('permissions',{_perms:args.perms,_mdls:args.models});
 //export const onModel = (...onModels: onModel[]) => SetMetadata('onModels', onModels);
