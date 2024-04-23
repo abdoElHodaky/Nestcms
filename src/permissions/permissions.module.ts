4365d5e402ea013c9e8c63bac48ad7df08472c63
AbdElrhman ElHodaky <47593167+abdoElHodaky@users.noreply.github.com>
2024-04-23 14:44:13 +0200

    Update permissions.module.ts

--- src/permissions/permissions.module.ts
+++ src/permissions/permissions.module.ts
@@ -1,7 +1,7 @@
 import { Module } from '@nestjs/common';
 import { PermissionService } from './permissions.service';
 import { APP_GUARD } from '@nestjs/core';
-import { PermissionController } from './permissions.cintroller';
+import { PermissionController } from './permissions.controller';
 //import { PermGuard} from "../perm.guard";
 import { MongooseModule } from '@nestjs/mongoose';
 import { PermissionSchema } from './models/permission.schema';
