f2657273fd2097af78f36396c82d5fc796aac801
AbdElrhman ElHodaky <47593167+abdoElHodaky@users.noreply.github.com>
2024-04-24 19:41:31 +0200

    Update article.interface.ts

--- src/articles/interfaces/article.interface.ts
+++ src/articles/interfaces/article.interface.ts
@@ -1,6 +1,25 @@
-export interface Article {
+
+import { Prop, Schema } from "@nestjs/mongoose";
+import  { HydratedDocument , Types} from "mongoose"
+
+export type ArticleDocument = HydratedDocument<Article>
+
+
+@Schema({
+    toJSON:{
+        versionKey: false,
+    },
+    toObject:{
+        versionKey: false,
+    }
+})
+export class Article {
+  @Prop()
   title: string;
+  @Prop()
   content: string;
-  author: string;
+  @Prop({type:Types.ObjectId,ref:"User"})
+  author: User;
+  @Prop()
   creationDate: string;
 }
