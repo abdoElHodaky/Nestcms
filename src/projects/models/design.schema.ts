254df835d0ffd4fd60ac23a516d5d88624a7369f
AbdElrhman ElHodaky <47593167+abdoElHodaky@users.noreply.github.com>
2024-04-24 19:36:39 +0200

    Update design.schema.ts

--- src/projects/models/design.schema.ts
+++ src/projects/models/design.schema.ts
@@ -1,9 +1,15 @@
-import * as mongoose from 'mongoose';
+import { SchemaFactory } from "@nestjs/mongoose";
+import { HydratedDocument } from "mongoose";
+import { Design } from "../interface/design";
 
-export const DesignSchema = new mongoose.Schema({
+/*export const DesignSchema = new mongoose.Schema({
   title: String,
   desc: String,
   path:String,
   project: { type: mongoose.Schema.Types.ObjectId, ref: 'Project'},
  // employee: { type: mongoose.Schema.Types.ObjectId, ref: 'User'}
-},{timestamps:true});
+},{timestamps:true});*/
+
+export const DesignSchema = SchemaFactory.createForClass(Design,{
+  timestamps:true
+});
