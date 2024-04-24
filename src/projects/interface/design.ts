6295d2396517de513c9c7cae7ba3b24c7068449a
AbdElrhman ElHodaky <47593167+abdoElHodaky@users.noreply.github.com>
2024-04-24 19:20:34 +0200

    Update design.ts

--- src/projects/interface/design.ts
+++ src/projects/interface/design.ts
@@ -1,10 +1,30 @@
 import { Project } from "./project";
-import _Design from "./design.d";
-export class Design implements _Design {
+//import _Design from "./design.d";
+import { Prop, Schema } from "@nestjs/mongoose";
+import  { HydratedDocument , Types} from "mongoose"
+
+export type DesignDocument = HydratedDocument<Design>
+
+
+@Schema({
+    toJSON:{
+        versionKey: false,
+    },
+    toObject:{
+        versionKey: false,
+    }
+})
+export class Design  {
+  @Prop()
   _id:string;
+  @Prop()
   desc:string;
+  @Prop()
   path:string;
+  @Prop()
   created_at?:string;
+  @Prop()
   updated_at?:string;
+  @Prop({type: Types.ObjectId,ref:"Project"})
   project?:Project;
 }
