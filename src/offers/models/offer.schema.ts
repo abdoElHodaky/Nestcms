5eea9f8fdbcf3758f3b50be64749b8c0d4744e16
AbdElrhman ElHodaky <47593167+abdoElHodaky@users.noreply.github.com>
2024-04-24 17:22:56 +0200

    Update offer.schema.ts

--- src/offers/models/offer.schema.ts
+++ src/offers/models/offer.schema.ts
@@ -2,7 +2,7 @@ import { SchemaFactory } from "@nestjs/mongoose";
 import { HydratedDocument } from "mongoose";
 import { Offer } from "../interface/offer.interface";
 
-export type OfferDocument = HydratedDocument<Offer>
+//export type OfferDocument = HydratedDocument<Offer>
 
 /*export const OfferSchema = new mongoose.Schema({
   title: String,
