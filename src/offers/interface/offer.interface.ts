a03e53565aecc55df9d035325eefd019661b4935
AbdElrhman ElHodaky <47593167+abdoElHodaky@users.noreply.github.com>
2024-04-24 16:40:28 +0200

    Update offer.interface.ts

--- src/offers/interface/offer.interface.ts
+++ src/offers/interface/offer.interface.ts
@@ -1,14 +1,35 @@
+import { Prop, Schema } from "@nestjs/mongoose";
+import mongoose from "mongoose"
 import { Client,Employee } from "../../users/interfaces/user";
 import { Contract } from "../../contracts/interface/contract";
-export interface Offer {
+
+@Schema({
+    toJSON:{
+        versionKey: false,
+    },
+    toObject:{
+        versionKey: false,
+    }
+})
+export class Offer {
+  @Prop()
   _id:string;
+  @Prop()
   title: string;
+  @Prop()
   content: string;
+  @Prop()
   creationDate: string;
+  @Prop()
   endDate:string;
+  @Prop()
   status:string;
+  @Prop()
   price:string;
+  @Prop({type:mongoose.Schema.Types.ObjectId,ref:"Contract"})
   contract?:Contract;
+  @Prop({type:mongoose.Schema.Types.ObjectId,ref:"User"})
   client?:Client;
+  @Prop({type:mongoose.Schema.Types.ObjectId,ref:"User"})
   employee?:Employee;
 }
