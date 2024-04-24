970b0cc27743673fa91fc23cb101d762f3d4b54d
AbdElrhman ElHodaky <47593167+abdoElHodaky@users.noreply.github.com>
2024-04-24 17:22:19 +0200

    Update payment.interface.ts

--- src/payments/interface/payment.interface.ts
+++ src/payments/interface/payment.interface.ts
@@ -4,6 +4,9 @@ import mongoose , {HydratedDocument} from "mongoose"
 import { Client } from "../../users/interfaces/user";
 import { Contract } from "../../contracts/interface/contract";
 //import _Payment from "./payment.d";
+
+export type PaymentDocument = HydratedDocument<Payment>
+    
 @Schema({
     toJSON:{
         versionKey: false,
