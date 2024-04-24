87d977bf026ccf660709a74dffb996b3cc673ebc
AbdElrhman ElHodaky <47593167+abdoElHodaky@users.noreply.github.com>
2024-04-24 17:00:13 +0200

    Update contract.schema.ts

--- src/contracts/models/contract.schema.ts
+++ src/contracts/models/contract.schema.ts
@@ -1,6 +1,10 @@
-import * as mongoose from 'mongoose';
+import { SchemaFactory } from "@nestjs/mongoose";
+import { HydratedDocument } from "mongoose";
+import { Contract } from "../interface/contract";
 
-export const ContractSchema = new mongoose.Schema({
+export type ContractDocument = HydratedDocument<Contract>
+
+/*export const ContractSchema = new mongoose.Schema({
   title: String,
   content: String,
   author: String,
@@ -11,4 +15,9 @@ export const ContractSchema = new mongoose.Schema({
   client: { type: mongoose.Schema.Types.ObjectId, ref: 'User'},
   employee: { type: mongoose.Schema.Types.ObjectId, ref: 'User'},
   payments: [{ type :mongoose.Schema.Types.ObjectId, ref :"Payment" }]
-},{timestamps:true});
+},{timestamps:true});*/
+
+export const ContractSchema = SchemaFactory.createForClass(Contract,{
+  timestamps:true
+});
+
