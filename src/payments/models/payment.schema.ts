c23288e8723e1c90297c029233adb7737a9c5d7f
AbdElrhman ElHodaky <47593167+abdoElHodaky@users.noreply.github.com>
2024-04-24 17:20:02 +0200

    Update payment.schema.ts

--- src/payments/models/payment.schema.ts
+++ src/payments/models/payment.schema.ts
@@ -1,6 +1,8 @@
-import * as mongoose from 'mongoose';
+import { SchemaFactory } from "@nestjs/mongoose";
+import { HydratedDocument } from "mongoose";
+import { Payment } from "../interface/payment";
 
-export const PaymentSchema = new mongoose.Schema({
+/*export const PaymentSchema = new mongoose.Schema({
   title: String,
   content: String,
   Date: String,
@@ -10,4 +12,8 @@ export const PaymentSchema = new mongoose.Schema({
   transR:String,
   client: { type: mongoose.Schema.Types.ObjectId, ref: 'User'},
   contract: { type: mongoose.Schema.Types.ObjectId, ref: 'Contract'}
-},{timestamps:true});
+},{timestamps:true}); */
+
+export const PaymentSchema = SchemaFactory.createForClass(Payment,{
+  timestamps:true
+});
