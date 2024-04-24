53e4444230faf9529ab46342e7299738290ef905
AbdElrhman ElHodaky <47593167+abdoElHodaky@users.noreply.github.com>
2024-04-24 19:23:30 +0200

    Update project-step.ts

--- src/projects/interface/project-step.ts
+++ src/projects/interface/project-step.ts
@@ -1,11 +1,31 @@
 import { Project } from "./project";
 //import { Contract } from "../contracts/contract.interface";
-import _ProjectStep from "./project-step.d";
-export class ProjectStep implements _ProjectStep {
+//import _ProjectStep from "./project-step.d";
+import { Prop, Schema } from "@nestjs/mongoose";
+import  { HydratedDocument , Types} from "mongoose"
+
+export type ProjectStepDocument = HydratedDocument<ProjectStep>
+
+
+@Schema({
+    toJSON:{
+        versionKey: false,
+    },
+    toObject:{
+        versionKey: false,
+    }
+})
+export class ProjectStep  {
+  @Prop()
   _id: string;
+  @Prop()
   startDate:string;
+  @Prop()
   endDate:string;
+  @Prop()
   content:string;
+  @Prop()
   status:string;
+  @Prop({type: Types.ObjectId, ref:"Project"})
   project?:Project
 }
