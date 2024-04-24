22987b0a4fd5affb3f93f00f9ecd1a655218b9c0
AbdElrhman ElHodaky <47593167+abdoElHodaky@users.noreply.github.com>
2024-04-24 17:35:12 +0200

    Update project.ts

--- src/projects/interface/project.ts
+++ src/projects/interface/project.ts
@@ -1,16 +1,39 @@
+import { Prop, Schema } from "@nestjs/mongoose";
+import mongoose , {HydratedDocument} from "mongoose"
+
+
 import { Design } from "./design";
 import { ProjectStep } from "./project-step";
 import { Contract } from "../../contracts/interface/contract";
 import { Employee } from "../../users/interfaces/user";
-import _Project from "./project.d"
-export class Project implements _Project {
+//import _Project from "./project.d"
+export type ProjectDocument = HydratedDocument<Projectt>
+    
+@Schema({
+    toJSON:{
+        versionKey: false,
+    },
+    toObject:{
+        versionKey: false,
+    }
+})
+export class Project  {
+  @Prop()
   _id: string;
+  @Prop()
   startDate:string;
+  @Prop()
   endDate:string;
+  @Prop()
   content:string;
+  @Prop()
   status:string;
+  @Prop({type:mongoose.Schema.Types.ObjectId,ref:"User"})
   employee?:Employee;
+  @Prop({type:[{type:mongoose.Schema.Types.ObjectId,ref:"designs"}]})
   designs?:Design[];
+  @Prop({type:mongoose.Schema.Types.ObjectId,ref:"Contract"})
   contract?:Contract;
+  @Prop({type:[{type:mongoose.Schema.Types.ObjectId,ref:"ProjectStep"}]})
   steps?:ProjectStep[];
 }
