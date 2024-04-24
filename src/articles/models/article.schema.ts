4c96e1ef0943d83ce2a2fde230634b7fb77f8cf2
AbdElrhman ElHodaky <47593167+abdoElHodaky@users.noreply.github.com>
2024-04-24 19:43:24 +0200

    Update article.schema.ts

--- src/articles/models/article.schema.ts
+++ src/articles/models/article.schema.ts
@@ -1,8 +1,15 @@
-import * as mongoose from 'mongoose';
+import { SchemaFactory } from "@nestjs/mongoose";
+import { HydratedDocument } from "mongoose";
+import { Article } from "../interface/article.interface";
 
+/*
 export const ArticleSchema = new mongoose.Schema({
   title: String,
   content: String,
   author: String,
   creationDate: String,
+});*/
+
+export const ArticleSchema = SchemaFactory.createForClass(Article,{
+  timestamps:true
 });
