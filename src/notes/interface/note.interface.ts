b4b7caa79b1f538c28f8c2558ad9bae323c37248
AbdElrhman ElHodaky <47593167+abdoElHodaky@users.noreply.github.com>
2024-04-24 18:54:04 +0200

    Update note.interface.ts

--- src/notes/interface/note.interface.ts
+++ src/notes/interface/note.interface.ts
@@ -1,8 +1,26 @@
+import { Prop, Schema } from "@nestjs/mongoose";
+import  {HydratedDocument , Types } from "mongoose"
+
 import { User } from "../../users/interfaces/user";
-export interface Note {
+export type NoteDocument = HydratedDocument<Note>
+
+@Schema({
+    toJSON:{
+        versionKey: false,
+    },
+    toObject:{
+        versionKey: false,
+    }
+})
+export class Note {
+  @Prop()
   _id:string;
+  @Prop()
   status:string;
+  @Prop()
   onId:string;
+  @Prop()
   onType:string;
+  @Prop()
   author?:User
 }
