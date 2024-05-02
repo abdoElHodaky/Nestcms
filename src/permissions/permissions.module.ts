ca1d1b1bcc3fa11085bac0c261be577bb87acb54
AbdElrhman ElHodaky <47593167+abdoElHodaky@users.noreply.github.com>
2024-05-02 09:21:40 +0300

    Update permissions.module.ts

--- src/permissions/permissions.module.ts
+++ src/permissions/permissions.module.ts
@@ -1,7 +1,7 @@
 import { Module } from '@nestjs/common';
 import { PermissionService } from './permissions.service';
 import { PermissionController } from './permissions.controller';
-import { UsersService} from "../../users/users.service";
+import { UsersService} from "../users/users.service";
 import { MongooseModule } from '@nestjs/mongoose';
 import { PermissionSchema } from './models/permission.schema';
 @Module({
