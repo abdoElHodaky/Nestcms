0575d0f0bca7cb4be3dca1887ddcfbbe23212c7f
AbdElrhman ElHodaky <abdo.arh38@yahoo.com>
2024-06-16 12:47:49 +0300

    Update create-project-schedule.dto.ts

--- src/schedules/dto/create-project-schedule.dto.ts
+++ src/schedules/dto/create-project-schedule.dto.ts
@@ -1,16 +1,26 @@
 import { ApiProperty, ApiPropertyOptional } from '@nestjs/swagger';
+import { IsNotEmpty, IsOptional } from 'class-validator';
 
 
 export class CreateProjectScheduleDto {
+   @IsNotEmpty()
    @ApiProperty()
-  readonly title: string;
+   readonly title: string;
+   
+   @IsOptional()
    @ApiPropertyOptional()
-  readonly content: string;
+   readonly content: string;
+
+   @IsNotEmpty()
    @ApiProperty()
-  readonly startDate: string;
+   readonly startDate: string;
+
+   @IsNotEmpty()
    @ApiProperty()
-  readonly endDate:string;
+    readonly endDate:string;
+
+   @IsOptional()
    @ApiProperty()
-  readonly projectId?:string;
+   readonly projectId?:string;
    
 }
