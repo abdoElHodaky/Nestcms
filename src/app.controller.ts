6fc7a09988dc8262761334ad4c4ee7f71b8b6f64
AbdElrhman ElHodaky <abdo.arh38@yahoo.com>
2024-06-16 19:03:33 +0300

    Update app.controller.ts

--- src/app.controller.ts
+++ src/app.controller.ts
@@ -1,14 +1,18 @@
 import { Controller, Get, UseGuards, Post, 
-        Request,Res ,Redirect} from '@nestjs/common';
-import { Response} from "express";
-@Controller('/')
+        Request,Res ,Redirect,UseGuards} from '@nestjs/common';
+//import { Response} from "express";
+import {OptionalJwtAuthGuard} from "perm.guard";
+@UseGuards(OptionalJwtAuthGuard)
+        
+@Controller()
 export class AppController {
   constructor() {}
   
   @Redirect("/docs")
+  
   @Get("")
   async index(@Res() res:Response ){   
-    res.redirect("docs")
+    
     
    }
 }
