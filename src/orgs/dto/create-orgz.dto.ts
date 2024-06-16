e38b6e609f8f0017dccfd8f48286e590362f1ce9
AbdElrhman ElHodaky <abdo.arh38@yahoo.com>
2024-06-16 12:15:56 +0300

    Update create-orgz.dto.ts

--- src/orgs/dto/create-orgz.dto.ts
+++ src/orgs/dto/create-orgz.dto.ts
@@ -1,15 +1,22 @@
 import { ApiProperty, ApiPropertyOptional } from '@nestjs/swagger';
+import { IsNotEmpty } from 'class-validator';
 
 export class CreateOrgzDto {
   
+  @IsNotEmpty()
   @ApiProperty()
   readonly title: string;
+  
+  @IsNotEmpty()
   @ApiProperty()
   readonly description: string;
+  
   @ApiPropertyOptional()
   readonly status:string;
   @ApiPropertyOptional()
   readonly ownerId:string;
+  
+  @IsNotEmpty()
   @ApiProperty()
   readonly address:{
     city:string,
