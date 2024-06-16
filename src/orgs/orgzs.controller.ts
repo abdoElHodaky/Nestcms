9bbd5f2412f24492f30f94e2e76433f2bb099201
AbdElrhman ElHodaky <abdo.arh38@yahoo.com>
2024-06-16 12:01:41 +0300

    Update orgzs.controller.ts

--- src/orgs/orgzs.controller.ts
+++ src/orgs/orgzs.controller.ts
@@ -1,1 +1,37 @@
+import { Controller, Post, Body, Get, Delete, Param, UseInterceptors, Put,UseGuards,Request } from '@nestjs/common';
+import { AuthGuard } from '@nestjs/passport';
+import { CreateNoteDto } from './dto/create-note.dto';
+//import { NoteService } from './notes.service';
+import { ApiTags,ApiSecurity,ApiBearerAuth } from "@nestjs/swagger";
 
+@ApiBearerAuth('JWTAuthorization')
+@ApiTags("Orgzs")
+@UseGuards(AuthGuard('jwt'))
+@Controller(["api/orgzs"])
+export class OrgzController {
+  constructor(/*private readonly noteService: NoteService*/) {}
+  @Post("create")
+  async create(/*@Body() createNoteDto: CreateNoteDto*/) {
+    return //await this.noteService.create(createNoteDto);
+  }
+  
+  
+/*
+
+  @Get()
+  async findAll() {
+    return this.articlesService.findAll();
+  }
+
+  @Get(':id')
+  async findOne(@Param('id') id: string) {
+    return this.articlesService.findOne(id);
+  }
+
+  @Delete(':id')
+  async deleteArticle(@Param('id') id: string) {
+    // tslint:disable-next-line: no-console
+    console.log('delete article with id -> ', id);
+    return this.articlesService.delete(id);
+  }*/
+}
