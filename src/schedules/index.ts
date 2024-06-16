66490381204db317cd55e99d0f38995af599d1c5
AbdElrhman ElHodaky <abdo.arh38@yahoo.com>
2024-06-16 23:13:26 +0300

    Create index.ts

---
+++ src/schedules/index.ts
@@ -0,0 +1,5 @@
+export {SchedulesModule} from "./schedules.module";
+export {Schedule} from "./interface/schedule";
+export {ScheduleProject} from "./interface/schedule-project";
+export {ScheduleProjectService} from "./schedules-projects.service"
+export {ScheduleService} from "./schedules.service";
