ab5be001111ca331f61b1464f7ec7e935d10e37d
AbdElrhman ElHodaky <abdo.arh38@yahoo.com>
2024-06-16 23:25:50 +0300

    Update index.ts

--- src/offers/index.ts
+++ src/offers/index.ts
@@ -1,3 +1,3 @@
 export {OffersModule} from "./offers.module";
 export {OfferService} from "./offers.service";
-export {Offer} from "./interface/offer";
+export {Offer} from "./interface/offer.interface";
