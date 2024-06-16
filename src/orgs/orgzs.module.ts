508d58b3990dc125d2f116062dc3e7924fdad521
AbdElrhman ElHodaky <abdo.arh38@yahoo.com>
2024-06-16 13:43:45 +0300

    Update orgzs.module.ts

--- src/orgs/orgzs.module.ts
+++ src/orgs/orgzs.module.ts
@@ -2,7 +2,7 @@ import { Module } from '@nestjs/common';
 import { OrgzService } from './orgzs.service';
 import { OrgzController } from './orgzs.controller';
 import { MongooseModule } from '@nestjs/mongoose';
-import { OrgzSchema } from './models/note.schema';
+import { OrgzSchema } from './models/orgz.schema';
 @Module({
   imports: [MongooseModule.forFeature([{ name: 'Orgz', schema: OrgzSchema }])],
   providers: [ OrgzService  ],
