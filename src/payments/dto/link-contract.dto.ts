0bea11c2fee895bdf923b330992247c9af78af6c
AbdElrhman ElHodaky <abdo.arh38@yahoo.com>
2024-06-16 15:34:25 +0300

    Update link-contract.dto.ts

--- src/payments/dto/link-contract.dto.ts
+++ src/payments/dto/link-contract.dto.ts
@@ -1,9 +1,17 @@
 import { ApiProperty, ApiPropertyOptional } from '@nestjs/swagger';
+import { IsNotEmpty, IsOptional } from 'class-validator';
+import { IsObjectId } from 'class-validator-mongo-object-id';
 
 export class PaymentLinkToContractDto {
+  
+  @IsNotEmpty()
+  @IsObjectId()
   @ApiProperty()
-   paymentId: string;
+  paymentId: string;
+
+  @IsNotEmpty()
+  @IsObjectId()
   @ApiProperty()
-   contractId: string;
+  contractId: string;
   
 }
