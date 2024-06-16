3040dceebd3127c05581296524e0c37dc63e93d7
AbdElrhman ElHodaky <abdo.arh38@yahoo.com>
2024-06-17 00:32:51 +0300

    Update address.ts

--- src/address/address.ts
+++ src/address/address.ts
@@ -1,19 +1,20 @@
-import { ApiHide } from "@nestjs/swagger";
+import { ApiHideProperty } from "@nestjs/swagger";
 
 export class Address {
    
+    @ApiHideProperty()
     title: string;
 
-   
+    @ApiHideProperty()
     street: string;
     
-   
+    @ApiHideProperty()
     city: string;
 
-   
+    @ApiHideProperty()
     state: string;
    
-    
+    @ApiHideProperty()
     country: string;
   
 }
