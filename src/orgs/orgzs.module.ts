beeed1bc3f417b96ad8d55272159f69b39a415ac
AbdElrhman ElHodaky <abdo.arh38@yahoo.com>
2024-06-16 11:53:43 +0300

    Update orgzs.module.ts

--- src/orgs/orgzs.module.ts
+++ src/orgs/orgzs.module.ts
@@ -1,1 +1,12 @@
-
+import { Module } from '@nestjs/common';
+//import { NoteService } from './notes.service';
+//import { NoteController } from './notes.controller';
+import { MongooseModule } from '@nestjs/mongoose';
+import { OrgzSchema } from './models/note.schema';
+@Module({
+  imports: [MongooseModule.forFeature([{ name: 'Orgz', schema: OrgzSchema }])],
+  providers: [],
+  exports: [],
+  controllers: [],
+})
+export class OrgzsModule {}
