621e390caa4cb0e0d29cfd0be130309acd2e10fc
AbdElrhman ElHodaky <abdo.arh38@yahoo.com>
2024-06-16 12:49:41 +0300

    Update login-user.dto.ts

--- src/users/dto/login-user.dto.ts
+++ src/users/dto/login-user.dto.ts
@@ -1,7 +1,13 @@
 import { ApiProperty, ApiPropertyOptional } from '@nestjs/swagger';
+import { IsNotEmpty, IsOptional } from 'class-validator';
+
 export class LoginUserDto {
+  
+  @IsNotEmpty()
   @ApiProperty()
   readonly email: string;
+  
+  @IsNotEmpty()
   @ApiProperty()
   readonly password: string;
 }
