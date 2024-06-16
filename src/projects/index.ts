806746c38388f7bef512dd0df345287859d8f646
AbdElrhman ElHodaky <abdo.arh38@yahoo.com>
2024-06-16 23:07:06 +0300

    Create index.ts

---
+++ src/projects/index.ts
@@ -0,0 +1,5 @@
+export {ProjectsModule} from "./projects.module";
+export {Project} from "./interface/project";
+export {Design} from "./interface/design";
+export {ProjectStep} from "./interface/project-step";
+export {ProjectService} from "./projects.service";
