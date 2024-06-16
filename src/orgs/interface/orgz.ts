2e09dd0a0d2b8ce9859e7bd4c9a7faa6042d7003
AbdElrhman ElHodaky <abdo.arh38@yahoo.com>
2024-06-16 11:48:06 +0300

    Update orgz.ts

--- src/orgs/interface/orgz.ts
+++ src/orgs/interface/orgz.ts
@@ -1,1 +1,39 @@
+import { Prop, Schema } from "@nestjs/mongoose";
+import  {HydratedDocument , Types } from "mongoose"
 
+import { User } from "../../users/interfaces/user";
+export type NoteDocument = HydratedDocument<Orgz>
+
+@Schema({
+    toJSON:{
+        versionKey: false,
+    },
+    toObject:{
+        versionKey: false,
+    }, timestamps:true
+})
+export class Orgz {
+  @Prop()
+  _id:string;
+  @Prop()
+  address:{
+    city:string,
+    street:string,
+    postalCode:string,
+    country:string
+  }
+  @Prop({
+      type:String,
+      enum:["open","closed"]
+  })
+  status:string;
+ /* @Prop({type:Types.ObjectId,refPath:"onModel",required:true})
+  onId:string;
+  @Prop({type:String,
+      required:true,
+      enum: ['Project', 'Design','Schedule']
+  })*/
+  //onModel:string;
+  @Prop({type:Types.ObjectId,ref:"User"})
+  owner?:User
+}
