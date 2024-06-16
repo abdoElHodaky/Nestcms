46189b12af610f7eb4683317992c7bb9e1bc3533
AbdElrhman ElHodaky <abdo.arh38@yahoo.com>
2024-06-16 12:56:45 +0300

    Update accept-offer.dto.ts

--- src/offers/dto/accept-offer.dto.ts
+++ src/offers/dto/accept-offer.dto.ts
@@ -1,11 +1,17 @@
 import { ApiProperty, ApiPropertyOptional } from '@nestjs/swagger';
+import { IsNotEmpty, IsOptional } from 'class-validator';
 
 export class AcceptOfferDto {
-  
+
+  @IsOptional()
   @ApiProperty()
   readonly status:string;
+  
+  @IsOptional()
   @ApiProperty()
   readonly offerId?:string;
+  
+  @IsOptional()
   @ApiProperty()
   readonly clientId?:string;
 }
