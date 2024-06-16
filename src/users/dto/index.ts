c59309e1c8111a82fb4055a988c3ac89ccf0fac6
AbdElrhman ElHodaky <abdo.arh38@yahoo.com>
2024-06-16 23:36:30 +0300

    Update index.ts

--- src/users/dto/index.ts
+++ src/users/dto/index.ts
@@ -1,2 +1,4 @@
 export { CreateUserDto } from './create-user.dto';
 export { LoginUserDto } from './login-user.dto';
+export { CreateClientDto} from "./create-client.dto";
+export { CreateEmployeeDto} from "./create-employee.dto";
