07f46b87bb5fa6ca5b8ffde8a311ded675511d3c
AbdElrhman ElHodaky <abdo.arh38@yahoo.com>
2024-06-16 13:12:40 +0300

    Update create-offer.dto.ts

--- src/offers/dto/create-offer.dto.ts
+++ src/offers/dto/create-offer.dto.ts
@@ -1,17 +1,31 @@
 import { ApiProperty, ApiPropertyOptional } from '@nestjs/swagger';
+import { IsObjectId } from 'class-validator-mongo-object-id';
+import { IsNotEmpty, IsOptional } from 'class-validator';
 
 export class CreateOfferDto {
   
   @ApiProperty()
+  @IsNotEmpty()
   readonly title: string;
+
+  @IsNotEmpty()
   @ApiProperty()
   readonly content: string;
+  
   @ApiProperty()
+  @IsNotEmpty()
   readonly startDate: string;
+
+  @IsNotEmpty()
   @ApiProperty()
   readonly endDate:string;
+
+  @IsOptional()
   @ApiProperty()
   readonly status:string;
+
+  @IsOptional()
+  @IsObjectId()
   @ApiPropertyOptional()
   readonly employeeId?:string;
 }
