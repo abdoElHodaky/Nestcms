a5c95ba25c97fbbb5ebcb8e1df5a1b261c79b3dc
AbdElrhman ElHodaky <abdo.arh38@yahoo.com>
2024-06-16 12:22:44 +0300

    Update create-note.dto.ts

--- src/notes/dto/create-note.dto.ts
+++ src/notes/dto/create-note.dto.ts
@@ -1,17 +1,28 @@
 import { ApiProperty, ApiPropertyOptional } from '@nestjs/swagger';
+import { IsNotEmpty, IsOptional } from 'class-validator';
 
 export class CreateNoteDto {
   
+  @IsNotEmpty()
   @ApiProperty()
   readonly title: string;
+  
+  @IsNotEmpty()
   @ApiProperty()
   readonly content: string;
+  
+  @IsOptional()
   @ApiPropertyOptional()
   readonly status:string;
+  
+  @IsNotEmpty()
   @ApiProperty()
   readonly onId:string
+  @IsNotEmpty()
   @ApiProperty()
   readonly onModel:string
+  
+  @IsOptional()
   @ApiProperty()
-  readonly authorId:string;
+  readonly authorId?:string;
 }
