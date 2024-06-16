1d418440793a1e91182a0d076a56c9cf60c7c106
AbdElrhman ElHodaky <abdo.arh38@yahoo.com>
2024-06-16 23:33:11 +0300

    Update index.ts

--- src/users/index.ts
+++ src/users/index.ts
@@ -1,3 +1,4 @@
 export {UsersModules} from "./users.module";
 export {UsersService} from "./users.service";
 export {User,Client,Employee,Admin} from "./interfaces/user";
+export { CreateUserDto, LoginUserDto  } from './dto/';
