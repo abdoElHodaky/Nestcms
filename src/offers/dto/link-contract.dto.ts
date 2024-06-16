a47463fa9bbaf934be28386bd9edf18ff8ca00c2
AbdElrhman ElHodaky <abdo.arh38@yahoo.com>
2024-06-16 13:13:51 +0300

    Update link-contract.dto.ts

--- src/offers/dto/link-contract.dto.ts
+++ src/offers/dto/link-contract.dto.ts
@@ -1,8 +1,16 @@
 import { ApiProperty, ApiPropertyOptional } from '@nestjs/swagger';
+import { IsObjectId } from 'class-validator-mongo-object-id';
+import { IsNotEmpty, IsOptional } from 'class-validator';
 
 export class OfferLinkToContractDto {
+  
+  @IsNotEmpty()
+  @IsObjectId()
   @ApiProperty()
   readonly offerId: string;
+  
+  @IsNotEmpty()
+  @IsObjectId()
   @ApiProperty()
   readonly contractId: string;
   
