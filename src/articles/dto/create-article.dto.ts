6a877c8f31a0c486c6b7c5797f91cb9265362dc3
AbdElrhman ElHodaky <abdo.arh38@yahoo.com>
2024-06-16 12:23:57 +0300

    Update create-article.dto.ts

--- src/articles/dto/create-article.dto.ts
+++ src/articles/dto/create-article.dto.ts
@@ -3,8 +3,14 @@ import { ApiProperty, ApiPropertyOptional } from '@nestjs/swagger';
 
 
 export class CreateArticleDto {
+  
+  @ApiProperty()
+  @IsNotEmpty()
   readonly title: string;
+  @ApiProperty()
+  @IsNotEmpty()
   readonly content: string;
-  readonly author?: string;
-  readonly creationDate?: string;
+  //readonly author?: string;
+  
+ // readonly creationDate?: string;
 }
