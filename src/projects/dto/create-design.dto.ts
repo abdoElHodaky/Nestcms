5db0404552704d786658a565a5e3edd1b1c8d287
AbdElrhman ElHodaky <abdo.arh38@yahoo.com>
2024-06-16 12:29:34 +0300

    Update create-design.dto.ts

--- src/projects/dto/create-design.dto.ts
+++ src/projects/dto/create-design.dto.ts
@@ -1,9 +1,14 @@
 import { ApiProperty, ApiPropertyOptional } from '@nestjs/swagger';
+import { IsNotEmpty, IsOptional } from 'class-validator';
 
 
 export class CreateDesignDto {
+  
+  @IsOptional()
   @ApiPropertyOptional()
   readonly desc:string;
+  
+  @IsNotEmpty()
   @ApiProperty()
   readonly path:string;
 }
