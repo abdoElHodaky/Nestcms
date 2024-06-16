1584cb53ba47e77329d1148d294ce675336c9a74
AbdElrhman ElHodaky <abdo.arh38@yahoo.com>
2024-06-16 12:20:42 +0300

    Update create-article.dto.ts

--- src/articles/dto/create-article.dto.ts
+++ src/articles/dto/create-article.dto.ts
@@ -1,3 +1,7 @@
+import { IsNotEmpty, IsOptional } from 'class-validator';
+import { ApiProperty, ApiPropertyOptional } from '@nestjs/swagger';
+
+
 export class CreateArticleDto {
   readonly title: string;
   readonly content: string;
