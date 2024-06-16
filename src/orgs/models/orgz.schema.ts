d78e4a5fddc7697edb48bfc7b261b8ff17107117
AbdElrhman ElHodaky <abdo.arh38@yahoo.com>
2024-06-16 11:51:51 +0300

    Update orgz.schema.ts

--- src/orgs/models/orgz.schema.ts
+++ src/orgs/models/orgz.schema.ts
@@ -1,1 +1,6 @@
+import { SchemaFactory } from "@nestjs/mongoose";
+import { HydratedDocument } from "mongoose";
+import { Orgz } from "../interface/orgz";
 
+
+export const NoteSchema = SchemaFactory.createForClass(Orgz);
