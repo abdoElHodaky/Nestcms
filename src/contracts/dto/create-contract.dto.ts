8ff50d7c459f68bbe184ada3e42ac4f253a092d4
AbdElrhman ElHodaky <abdo.arh38@yahoo.com>
2024-06-16 12:27:03 +0300

    Update create-contract.dto.ts

--- src/contracts/dto/create-contract.dto.ts
+++ src/contracts/dto/create-contract.dto.ts
@@ -1,25 +1,42 @@
 import { ApiProperty, ApiPropertyOptional } from '@nestjs/swagger';
+import { IsNotEmpty, IsOptional } from 'class-validator';
 
 export class CreateContractDto {
   
   @ApiProperty()
+  @IsNotEmpty()
   readonly title: string;
+  
   @ApiProperty()
+  @IsNotEmpty()
   readonly content: string;
+  @IsOptional()
   @ApiPropertyOptional()
   readonly author: string;
+  @IsNotEmpty()
   @ApiProperty()
   readonly startDate: string;
+  
+  @IsNotEmpty()
   @ApiProperty()
   readonly endDate:string;
+  
+  @IsOptional()
   @ApiProperty()
   readonly status:string;
+  
+  @IsNotEmpty()
   @ApiPropertyOptional()
   readonly path?:string
+  
+  @IsNotEmpty()
   @ApiPropertyOptional()
   readonly employeeId?:string;
+  @IsOptional()
   @ApiPropertyOptional()
   readonly clientId?:string;
+  
+  @IsNotEmpty()
   @ApiPropertyOptional()
   readonly offerId?:string
 
