8494a048e890e13bd587c6ab32573ea0befe615f
AbdElrhman ElHodaky <abdo.arh38@yahoo.com>
2024-06-16 12:32:13 +0300

    Update create-project.dto.ts

--- src/projects/dto/create-project.dto.ts
+++ src/projects/dto/create-project.dto.ts
@@ -1,16 +1,28 @@
 import { ApiProperty, ApiPropertyOptional } from '@nestjs/swagger';
+import { IsNotEmpty, IsOptional } from 'class-validator';
 
 export class CreateProjectDto {
+  
+  @IsNotEmpty()
   @ApiProperty()
   readonly title: string;
+  
+  @IsNotEmpty()
   @ApiProperty()
   readonly content: string;
+  
+  @IsNotEmpty()
   @ApiProperty()
   readonly startDate: string;
+  @IsNotEmpty()
   @ApiProperty()
   readonly endDate:string;
+  
+  @IsOptional()
   @ApiPropertyOptional()
   readonly status:string;
+  
+  @IsOptional()
   @ApiPropertyOptional()
   readonly employeeId?:string;
   
