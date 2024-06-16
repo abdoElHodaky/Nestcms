1719997bbf071751f920f7ae628312d51592e8b1
AbdElrhman ElHodaky <abdo.arh38@yahoo.com>
2024-06-16 15:29:27 +0300

    Update create-payment.dto.ts

--- src/payments/dto/create-payment.dto.ts
+++ src/payments/dto/create-payment.dto.ts
@@ -1,20 +1,36 @@
 import { ApiProperty, ApiPropertyOptional } from '@nestjs/swagger';
+import { IsNotEmpty, IsOptional,IsDate,IsInt } from 'class-validator';
+import { IsObjectId } from 'class-validator-mongo-object-id';
 
 export class CreatePaymentDto {
+  
+  @IsNotEmpty()
+  @IsObjectId()
   @ApiProperty()
   readonly contractId:string;
  /* @ApiProperty()
   readonly clientId:string; */
+  @IsNotEmpty()
   @ApiProperty()
   readonly title: string;
+
+  @IsNotEmpty()
   @ApiProperty()
   readonly content: string;
+
+  @IsDate()
   @ApiProperty()
   readonly Date:string
+
+  @IsOptional()
   @ApiPropertyOptional()
   readonly status:string;
+
+  @IsInt()
   @ApiProperty()
   readonly amount:number;
+  
+  @IsNotEmpty()
   @ApiProperty()
   readonly currency:string;
  
