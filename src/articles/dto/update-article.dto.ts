b3655e3375a30d5ddb12ad0a32fc900faf051e15
AbdElrhman ElHodaky <abdo.arh38@yahoo.com>
2024-06-17 08:59:07 +0300

    Update update-article.dto.ts

--- src/articles/dto/update-article.dto.ts
+++ src/articles/dto/update-article.dto.ts
@@ -1,6 +1,13 @@
+import { IsNotEmpty, IsOptional } from 'class-validator';
+import { ApiProperty, ApiPropertyOptional } from '@nestjs/swagger';
+
 export class UpdateArticleDto {
+  @ApiProperty()
+  @IsNotEmpty()
   readonly title: string;
+  
+  @ApiProperty()
+  @IsNotEmpty()
   readonly content: string;
-  readonly author?: string;
-  readonly creationDate?: string;
+
 }
