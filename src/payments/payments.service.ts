da9e6241caebbe582f7dc68f565c91c40190be00
AbdElrhman ElHodaky <abdo.arh38@yahoo.com>
2024-10-04 14:32:57 +0300

    Update payments.service.ts

--- src/payments/payments.service.ts
+++ src/payments/payments.service.ts
@@ -48,7 +48,7 @@ export class PaymentService {
     const {paymentId,contractId}=paymentLinkToContractDto
     const payment=await this.paymModel.findById(paymentId)
     const contract=await this.contractService.find_Id(contractId)
-    payment.contractId=contract._id
+    payment.contractId=new Types.ObjectId(contract)
     return await payment.save()
   }
   async Pay(paymentId:string,urls:{callback:string,return:string}):Promise<any>{
