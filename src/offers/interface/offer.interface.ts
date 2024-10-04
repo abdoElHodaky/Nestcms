364df962871c45e8646b9c611fbe9389021b7ee9
AbdElrhman ElHodaky <abdo.arh38@yahoo.com>
2024-10-04 19:24:18 +0300

    Update offer.interface.ts

--- src/offers/interface/offer.interface.ts
+++ src/offers/interface/offer.interface.ts
@@ -30,7 +30,7 @@ export class Offer {
   @Prop()
   price:string;
   @Prop({type:mongoose.Schema.Types.ObjectId,ref:"Contract"})
-  contract?:_Contract;
+  contract:_Contract;
   @Prop({type:mongoose.Schema.Types.ObjectId,ref:"User"})
   client?:Client;
   @Prop({type:mongoose.Schema.Types.ObjectId,ref:"User"})
