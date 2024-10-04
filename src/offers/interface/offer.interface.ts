c8625c491113f340423cca4692c835b20d26ef72
AbdElrhman ElHodaky <abdo.arh38@yahoo.com>
2024-10-04 13:57:25 +0300

    Update offer.interface.ts

--- src/offers/interface/offer.interface.ts
+++ src/offers/interface/offer.interface.ts
@@ -1,7 +1,7 @@
 import { Prop, Schema } from "@nestjs/mongoose";
 import mongoose , {HydratedDocument} from "mongoose"
 import { Client,Employee } from "../../users/interfaces/user";
-import { Contract } from "../../contracts/";
+const { Contract } =await import( "../../contracts/");
 
 //export type OfferDocument = HydratedDocument<Offer>
 
