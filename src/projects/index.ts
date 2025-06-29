ba03c192f0ea4cf4483410a3e5eafdeac7b1cebb
AbdElrhman ElHodaky <abdo.arh38@yahoo.com>
2025-06-29 09:03:26 +0300

    Update index.ts

--- src/projects/index.ts
+++ src/projects/index.ts
@@ -1,5 +1,6 @@
 export {ProjectsModule} from "./projects.module";
 export {Project} from "./interface/project";
 export {Design} from "./interface/design";
+export {ProjectWorker} from "./interface/worker"
 export {ProjectStep} from "./interface/project-step";
 export {ProjectService} from "./projects.service";
