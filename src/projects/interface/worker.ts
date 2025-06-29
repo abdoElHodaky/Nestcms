d00605e544beaccb079646f5afe72439277ffb97
AbdElrhman ElHodaky <abdo.arh38@yahoo.com>
2025-06-29 08:56:21 +0300

    Update worker.ts

--- src/projects/interface/worker.ts
+++ src/projects/interface/worker.ts
@@ -1,1 +1,23 @@
+import { Project } from "./project";
+import {Employee} from "../../users/";
+import { Prop, Schema } from "@nestjs/mongoose";
+import  { HydratedDocument , Types} from "mongoose"
+
+//export type DesignDocument = HydratedDocument<Design>
+
+
+@Schema({
+    toJSON:{
+        versionKey: false,
+    },
+    toObject:{
+        versionKey: false,
+    }, timestamps:true
+})
+export class Worker extends Employee  {
+  @Prop()
+  role:string;
+  @Prop({type: Types.ObjectId,ref:"Project"})
+  project?:Project;
+}
 
