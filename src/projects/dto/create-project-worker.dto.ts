eb1264bfa8ae58e0c793fce8cf13f5217534715c
AbdElrhman ElHodaky <abdo.arh38@yahoo.com>
2025-06-30 16:48:46 +0300

    Update create-project-worker.dto.ts

--- src/projects/dto/create-project-worker.dto.ts
+++ src/projects/dto/create-project-worker.dto.ts
@@ -2,13 +2,17 @@ import { PartialType } from '@nestjs/mapped-types';
 import { ApiProperty, ApiPropertyOptional } from '@nestjs/swagger';
 import { IsNotEmpty, IsOptional } from 'class-validator';
 import { IsObjectId } from 'class-validator-mongo-object-id';
-import {CreateEmployeeDto} from "../../users/dto"
-export class CreateProjectWorkerDto extends PartialType(CreateEmployeeDto) {
+//import {CreateEmployeeDto} from "../../users/dto"
+export class CreateProjectWorkerDto  {
   
   @IsNotEmpty()
   @IsObjectId()
   @ApiProperty()
   readonly projectId:string;
+
+  @IsNotEmpty()
+  @ApiProperty()
+  readonly role:string;
   
   
 }
