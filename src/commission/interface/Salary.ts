f2e3304ae1a7f2f2cdc1f843e168785681b807ef
AbdElrhman ElHodaky <abdo.arh38@yahoo.com>
2025-07-01 12:09:56 +0300

    Update Salary.ts

--- src/commission/interface/Salary.ts
+++ src/commission/interface/Salary.ts
@@ -1,1 +1,25 @@
+import { Prop, Schema } from "@nestjs/mongoose";
+import  { HydratedDocument , Types} from "mongoose"
+import { Employee} from "../../users/interfaces/user";
+//export type ArticleDocument = HydratedDocument<Article>
 
+
+@Schema({
+    toJSON:{
+        versionKey: false,
+    },
+    toObject:{
+        versionKey: false,
+    },
+    timestamps:true
+})
+export class Salary {
+  @Prop()
+  title: string;
+  @Prop()
+  amount: number;
+  @Prop({type:Types.ObjectId,ref:"User"})
+  employee: Employee;
+  @Prop()
+  payoutDate: Date;
+}
