9a4caf0577fa95166930ff8c04939214000edd6d
AbdElrhman ElHodaky <abdo.arh38@yahoo.com>
2025-07-01 12:36:24 +0300

    Update commission.service.ts

--- src/commission/commission.service.ts
+++ src/commission/commission.service.ts
@@ -1,1 +1,39 @@
+import { Injectable } from '@nestjs/common';
+import { Model } from 'mongoose';
+import { InjectModel } from '@nestjs/mongoose';
+import { Commission } from './interfaces/salary';
+//import { CreateArticleDto ,  UpdateArticleDto } from './dto/';
+//import { UpdateArticleDto } from './dto/update-article.dto';
 
+@Injectable()
+export class CommissionService {
+  constructor(
+    @InjectModel('Commission') private readonly salaryModel: Model<Commission>,
+  ) {}
+
+ /* async create(createArticleDto: CreateArticleDto): Promise<Article> {
+    const createdArticle = new this.articleModel(createArticleDto);
+    return await createdArticle.save();
+  }
+
+  async findAll(): Promise<Article[]> {
+    return await this.articleModel
+      .find()
+      .sort({ creationDate: -1 })
+      .exec();
+  }
+
+  async findOne(id: string): Promise<Article> {
+    return await this.articleModel.findById(id).exec();
+  }
+
+ /* async delete(id: string): Promise<Article> {
+    return await this.articleModel.findByIdAndRemove(id);
+  }
+
+  async update(id: string, article: UpdateArticleDto) {
+    const updatedArticle = this.articleModel.findByIdAndUpdate(id, article, { new: true });
+    console.log(updatedArticle);
+    return updatedArticle;
+  }*/
+}
