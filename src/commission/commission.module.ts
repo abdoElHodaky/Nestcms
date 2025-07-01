f4a6274ae9a028424cec0ae9ddca1a1ad8bc33b5
AbdElrhman ElHodaky <abdo.arh38@yahoo.com>
2025-07-01 12:18:05 +0300

    Update commission.module.ts

--- src/commission/commission.module.ts
+++ src/commission/commission.module.ts
@@ -1,1 +1,15 @@
+import { Module } from '@nestjs/common';
+import { MongooseModule } from '@nestjs/mongoose';
+import { SalarySchema , CommissionSchema} from './models/schema';
+//import { ArticlesService } from './articles.service';
+//import { ArticlesController } from './articles.controller';
 
+@Module({
+  imports: [
+    MongooseModule.forFeature([{ name: 'Salary', schema: SalarySchema },
+                               { name: "Commission", schema: CommissionSchema}]),
+  ],
+  providers: [],
+  controllers: [],
+})
+export class CommSalaryModule {}
