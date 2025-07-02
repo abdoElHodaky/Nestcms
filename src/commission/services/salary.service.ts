a825d750684d5927a265e21070c656f3fe69662c
AbdElrhman ElHodaky <abdo.arh38@yahoo.com>
2025-07-02 11:04:28 +0300

    Update salary.service.ts

--- src/commission/services/salary.service.ts
+++ src/commission/services/salary.service.ts
@@ -1,7 +1,7 @@
 import { Injectable } from '@nestjs/common';
 import { Model } from 'mongoose';
 import { InjectModel } from '@nestjs/mongoose';
-import { Salary } from './interface/';
+import { Salary } from '../interface/';
 
 @Injectable()
 export class SalaryService {
