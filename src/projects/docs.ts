e180ce5b84cad1036048305c70e46db69a794c0c
AbdElrhman ElHodaky <abdo.arh38@yahoo.com>
2025-07-03 14:04:01 +0300

    Update docs.ts

--- src/projects/docs.ts
+++ src/projects/docs.ts
@@ -19,7 +19,7 @@ const options = new DocumentBuilder()
       )
     .addServer('/', '')
     .addTag('P',"projects endpoints")
-    .addTag('P.Schedule'," schedules endpoints for specific project")
+   // .addTag('P.Schedule'," schedules endpoints for specific project")
     .addTag('P.Step'," project steps endpoints")
     .addTag('P.Design',"designs endpoints of specific project")
     .addTag('P.Note','projects notes endpoints')
@@ -32,7 +32,7 @@ const options = new DocumentBuilder()
   const theme = new SwaggerTheme();
   SwaggerModule.setup('docs/projects', app, document,{
       //include:ProjectsModule,
-      swaggerOptions:{deepLinking: true},
+     // swaggerOptions:{deepLinking: true},
     customCss: theme.getBuffer(SwaggerThemeNameEnum.DARK),
     customSiteTitle:"Endpoints of construction company",
     customfavIcon:" data:image/png;base64,iVBORw0KGgoAAAANSUhEUgAAABAAAAAQCAYAAAAf8/9hAAAAAXNSR0IArs4c6QAAAjxJREFUOE+Vkk9I02EYx7/fd/stppHl3EYHK8JqP9H+MCLqUmH7lXXvFFK3IgiPhd6USupgF6ODSZ4UgqJLuS1ah04RhNK2/pFIJfstTbMcc/u9T2wOsYjU5/Twvt/v5/3yPg+xrGwzcvJNsBA9mkgUl5//r+fyy4xp9UHkYTAdi64JYJtWJ0QuAJgmpd+fiveuGvC+oXVDjVH8CkF12STo8b+NXWGpW0XRNiPDEJxe0lLuUqTHn376Thpa1/HD4/y/ONlQy84ijAVmzciECOoroi8Ax1yKA7XJg/ezoReXCRyAeM+IyrUpcU06dITATkJ8WnOUdigyAODsX690TFRP39gy74sL2O+GTDqidwlYC6pxJTKtIfsV+IiZ5pbtrrw3r135VxAEyyDiViAVa7fNyHUoldCOs0ORhyAYA7GXwkGQDULX0NIYv5lWlxbpXPxH3AmmY+ftkHXVBUlooIlkri4VvT3VeOycI7xEqHt/AhojLVojvpiAfYFU9KLdaF0TLc8UVJ2Gc9jR3g63OxeCqCMEfwIYLifImMfbDK0/FoknANaD0j1bMLprjGICwBDIOWhpF3CERJ7EDASfRbCtDLDNSGlxwgCqIdgnkBOBdDya2W1VBYs1hSSAgPzwFao8s15nzpObh3vzHv/38ZdZowKwuiqb+JxEWFhoCiQTpYgrVhkw1WTVa0hQHAwqUQ/q0iMdKzorgqUpSDhsZH/5bno86N04Gv20ZsBM86lNC87C1kAy9nq15pLuNwsR8zwBBPTJAAAAAElFTkSuQmCC "
