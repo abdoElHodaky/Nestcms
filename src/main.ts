15df3c4477261c9474dd993bd68723434ff2e25d
AbdElrhman ElHodaky <abdo.arh38@yahoo.com>
2025-07-03 13:20:23 +0300

    Update main.ts

--- src/main.ts
+++ src/main.ts
@@ -40,7 +40,7 @@ const options = new DocumentBuilder()
   const document = SwaggerModule.createDocument(app, options);
   const theme = new SwaggerTheme();
   SwaggerModule.setup('docs', app, document,{
-   swaggerOptions:{deepLinking: true},
+  // swaggerOptions:{deepLinking: true},
     explorer: false,
     customCss: theme.getBuffer(SwaggerThemeNameEnum.ONE_DARK),
     customSiteTitle:"Endpoints of construction company",
