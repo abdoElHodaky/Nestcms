194da6a55d46d4d7d48ccbc543a23bff5b0a13b7
AbdElrhman ElHodaky <abdo.arh38@yahoo.com>
2025-07-03 14:20:44 +0300

    Update worker.controller.ts

--- src/projects/controllers/worker.controller.ts
+++ src/projects/controllers/worker.controller.ts
@@ -5,7 +5,7 @@ import { CreateProjectWorkerDto } from "../dto/create-project-worker.dto";
 import { ApiTags,ApiSecurity,ApiBearerAuth,ApiOperation } from "@nestjs/swagger";
 @ApiBearerAuth('JWTAuthorization')
 //@ApiSecurity("bearer")
-@ApiTags("P.Worker")
+@ApiTags("Project.Worker")
 @UseGuards(AuthGuard('jwt'))
 @Controller('projects/:id/workers')
 export class WorkerController {
