58eb168bbdd41267e6fc54271845b2c73ff853e3
AbdElrhman ElHodaky <abdo.arh38@yahoo.com>
2025-07-04 11:08:00 +0300

    Update modules.app.ts

--- src/modules.app.ts
+++ src/modules.app.ts
@@ -10,8 +10,9 @@ import { PermissionsModule } from "./permissions/";
 import { ArticlesModule } from "./articles/";
 import { OrgzsModule} from "./orgs/";
 import { CommSalaryModule} from "./commission/"
-
+import { EarningsModule} from "./earnings/earnings.module";
 export const modules=[PermissionsModule,AuthModule,ContractsModule,
                UsersModule,SchedulesModule,PaymentsModule,
                ProjectsModule,OffersModule,OrgzsModule,
-               NotesModule,ArticlesModule,CommSalaryModule]
+               NotesModule,ArticlesModule,CommSalaryModule,
+               EarningsModule ]
