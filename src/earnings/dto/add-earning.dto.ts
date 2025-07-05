293a42a812ba5f7a7f068ba74bb98ce49511dc6a
AbdElrhman ElHodaky <abdo.arh38@yahoo.com>
2025-07-05 09:47:35 +0300

    Update add-earning.dto.ts

--- src/earnings/dto/add-earning.dto.ts
+++ src/earnings/dto/add-earning.dto.ts
@@ -1,1 +1,32 @@
+import { ApiProperty, ApiPropertyOptional } from '@nestjs/swagger';
+import { IsNotEmpty, IsOptional } from 'class-validator';
+//import { IsObjectId } from 'class-validator-mongo-object-id';
 
+export class AddEarningDto {
+  
+  @IsNotEmpty()
+  @ApiProperty()
+  readonly title: string;
+  
+  @IsNotEmpty()
+  @ApiProperty()
+  readonly amount: number;
+  
+  @IsOptional()
+  @ApiPropertyOptional()
+  readonly status:string;
+  
+  @IsNotEmpty()
+  @IsObjectId()
+  @ApiProperty()
+  readonly forType:string
+  
+  
+  @IsOptional()
+  @ApiProperty()
+  readonly announced_date:string;
+
+  @IsNotEmpty()
+  @ApiProperty()
+  readonly announced_periodically:string
+}
