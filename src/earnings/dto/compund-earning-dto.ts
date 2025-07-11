f375273e0e740b53eb2dec614db8bac1e64c4974
AbdElrhman ElHodaky <abdo.arh38@yahoo.com>
2025-07-11 12:23:23 +0300

    Update compund-earning-dto.ts

--- src/earnings/dto/compund-earning-dto.ts
+++ src/earnings/dto/compund-earning-dto.ts
@@ -1,1 +1,20 @@
+import { ApiProperty, ApiPropertyOptional } from '@nestjs/swagger';
+import { IsNotEmpty, IsOptional } from 'class-validator';
+import { IsObjectId } from 'class-validator-mongo-object-id';
 
+export class CoumpondEarningDto {
+  
+  @IsNotEmpty()
+  @ApiProperty({
+    type:String,
+    example:"project",
+    enum:["project","orgz"]
+  })
+  readonly type: string;
+
+  @IsNotEmpty()
+  @IsObjectId()
+  @ApiProperty()
+  readonly Id: string;
+  
+}
